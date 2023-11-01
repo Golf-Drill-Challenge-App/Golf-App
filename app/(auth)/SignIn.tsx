9a91fdb06d6c73d05e46329e1418ca2dd060f47a
Frankreed <baoanhtdn@gmail.com>
2023-10-31 20:01:28 -0700

    Fixed logins, removed player/coach selections, and added dependency for tunneling

--- app/(auth)/SignIn.tsx
+++ app/(auth)/SignIn.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { KeyboardAvoidingView, StyleSheet } from "react-native";
 import {
   Pressable,
   Text,
@@ -9,48 +9,78 @@ import {
 import { useAuth } from "../../context/Auth";
 import { Image } from "expo-image";
 import { Link } from "expo-router";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+import { useState } from "react";
 
 export default function SignIn() {
   const { signIn } = useAuth();
   const accentColor = useThemeColor({}, "accent");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const textColor = useThemeColor({}, "text");
 
+  async function handleSubmit() {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+      );
+      console.log(userCredential.user);
+      signIn();
+    } catch (e) {
+      console.error("Error signing user up:", e);
+    }
+  }
   return (
-    <View style={styles.container}>
-      <View style={styles.section}>
-        <Image
-          style={styles.image}
-          source={require("../../assets/images/appLogo.png")}
-          contentFit="contain"
-          contentPosition="center"
-        />
-        <Text style={[styles.title, { color: accentColor }]}>
-          Oregon State Golf
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.section}>
+          <Image
+              style={styles.image}
+              source={require("../../assets/images/appLogo.png")}
+              contentFit="contain"
+              contentPosition="center"
+          />
+          <Text style={[styles.title, { color: accentColor }]}>
+            Oregon State Golf
+          </Text>
+        </View>
+        <KeyboardAvoidingView behavior="padding">
+          <View style={styles.section}>
+            {/* TODO: Refactor TextInput into Themed */}
+            <TextInput
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect={false}
+                onChangeText={setEmail}
+                style={[{ color: textColor }, styles.input]}
+                placeholder="Email"
+            />
+            <TextInput
+                autoCapitalize="none"
+                autoComplete="password-new"
+                autoCorrect={false}
+                secureTextEntry={true}
+                onChangeText={setPassword}
+                style={[{ color: textColor }, styles.input]}
+                placeholder="Password"
+            />
+            <Text style={{ alignSelf: "flex-start" }}>Forgot your password?</Text>
+            <Pressable
+                style={[styles.button, { backgroundColor: accentColor }]}
+                onPress={handleSubmit}
+            >
+              <Text style={styles.buttonText}>Login</Text>
+            </Pressable>
+            <Link asChild href={"/SignUp"}>
+              <Pressable style={[styles.button, { backgroundColor: accentColor }]}>
+                <Text style={styles.buttonText}>Sign Up</Text>
+              </Pressable>
+            </Link>
+          </View>
+        </KeyboardAvoidingView>
       </View>
-      <View style={styles.section}>
-        {/* TODO: Refactor TextInput into Themed */}
-        <TextInput style={styles.input} placeholder="Email" />
-        <TextInput style={styles.input} placeholder="Password" />
-        <Text style={{ alignSelf: "flex-start" }}>Forgot your password?</Text>
-        <Pressable
-          style={[styles.button, { backgroundColor: accentColor }]}
-          onPress={() => signIn("player")}
-        >
-          <Text style={styles.buttonText}>Login (Player)</Text>
-        </Pressable>
-        <Pressable
-          style={[styles.button, { backgroundColor: accentColor }]}
-          onPress={() => signIn("coach")}
-        >
-          <Text style={styles.buttonText}>Login (Coach)</Text>
-        </Pressable>
-        <Link asChild href={"/SignUp"}>
-          <Pressable style={[styles.button, { backgroundColor: accentColor }]}>
-            <Text style={styles.buttonText}>Sign Up</Text>
-          </Pressable>
-        </Link>
-      </View>
-    </View>
   );
 }
 
--- app/(auth)/SignUp.tsx
+++ app/(auth)/SignUp.tsx
@@ -7,7 +7,7 @@ import {
 } from "../../components/Themed";
 import { Image } from "expo-image";
 import { useAuth } from "../../context/Auth";
-import { StyleSheet } from "react-native";
+import { KeyboardAvoidingView, StyleSheet } from "react-native";
 import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebaseConfig";
@@ -22,7 +22,6 @@ export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
-  const [userType, setUserType] = useState("Player");
 
 
   async function handleSubmit() {
@@ -33,80 +32,74 @@ export default function SignUp() {
 
     try {
       const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
+          auth,
+          email,
+          password
       );
       console.log(userCredential.user);
-      signIn("coach");
+      signIn();
     } catch (e) {
       console.error("Error signing user up:", e);
     }
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.section}>
-        <Image
-          style={styles.image}
-          source={require("../../assets/images/appLogo.png")}
-          contentFit="contain"
-          contentPosition="center"
-        />
-        <Text style={[styles.title, { color: accentColor }]}>
-          Oregon State Golf
-        </Text>
+      <View style={styles.container}>
+        <KeyboardAvoidingView behavior="padding">
+          <View style={styles.section}>
+            <Image
+                style={styles.image}
+                source={require("../../assets/images/appLogo.png")}
+                contentFit="contain"
+                contentPosition="center"
+            />
+            <Text style={[styles.title, { color: accentColor }]}>
+              Oregon State Golf
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <TextInput
+                autoCapitalize="none"
+                autoComplete="name"
+                onChangeText={setName}
+                style={[{ color: textColor }, styles.input]}
+                placeholder="Name"
+            />
+            <TextInput
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect={false}
+                onChangeText={setEmail}
+                style={[{ color: textColor }, styles.input]}
+                placeholder="Email"
+            />
+            <TextInput
+                autoCapitalize="none"
+                autoComplete="password-new"
+                autoCorrect={false}
+                secureTextEntry={true}
+                onChangeText={setPassword}
+                style={[{ color: textColor }, styles.input]}
+                placeholder="Password"
+            />
+            <TextInput
+                autoCapitalize="none"
+                autoComplete="password-new"
+                autoCorrect={false}
+                secureTextEntry={true}
+                onChangeText={setPasswordCheck}
+                style={[{ color: textColor }, styles.input]}
+                placeholder="Confirm Password"
+            />
+            <Pressable
+                style={[styles.button, { backgroundColor: accentColor }]}
+                onPress={handleSubmit}
+            >
+              <Text style={styles.buttonText}>Submit</Text>
+            </Pressable>
+          </View>
+        </KeyboardAvoidingView>
       </View>
-      <View style={styles.section}>
-        <SelectorButton
-          items={[
-            { label: "Coach", value: "Coach" },
-            { label: "Player", value: "Player" },
-          ]}
-          value={userType}
-          setValue={setUserType}
-        />
-        <TextInput
-          autoCapitalize="none"
-          autoComplete="name"
-          onChangeText={setName}
-          style={[{ color: textColor }, styles.input]}
-          placeholder="Name"
-        />
-        <TextInput
-          autoCapitalize="none"
-          autoComplete="email"
-          autoCorrect={false}
-          onChangeText={setEmail}
-          style={[{ color: textColor }, styles.input]}
-          placeholder="Email"
-        />
-        <TextInput
-          autoCapitalize="none"
-          autoComplete="password-new"
-          autoCorrect={false}
-          secureTextEntry={true}
-          onChangeText={setPassword}
-          style={[{ color: textColor }, styles.input]}
-          placeholder="Password"
-        />
-        <TextInput
-          autoCapitalize="none"
-          autoComplete="password-new"
-          autoCorrect={false}
-          secureTextEntry={true}
-          onChangeText={setPasswordCheck}
-          style={[{ color: textColor }, styles.input]}
-          placeholder="Confirm Password"
-        />
-        <Pressable
-          style={[styles.button, { backgroundColor: accentColor }]}
-          onPress={handleSubmit}
-        >
-          <Text style={styles.buttonText}>Submit</Text>
-        </Pressable>
-      </View>
-    </View>
   );
 }
 
