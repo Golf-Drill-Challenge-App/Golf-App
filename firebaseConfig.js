941109e03c172b1867ecd0c327cb1719a4c677cc
solderq35 <solderq35@gmail.com>
2024-01-15 19:13:29 -0800

    fix dependency issues and update async storage

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,8 +23,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+const storage = getStorage(app);
 const db = getFirestore(app);
 
-// connectFirestoreEmulator(db, "localhost", 8080);
-
-module.exports = db;
+module.exports = { auth, storage, db };
