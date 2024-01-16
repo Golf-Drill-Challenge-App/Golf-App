4004e02d0d24ddaadeaee9873096818378261efe
Frankreed <baoanhtdn@gmail.com>
2024-01-16 11:48:43 -0800

    replaced gluestack with react native paper

--- app/index.js
+++ app/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed"
-import { config } from "@gluestack-ui/config"
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { PaperProvider, Text } from 'react-native-paper';
 
 export default function Index() {
     return (
-        <GluestackUIProvider config={config}>
+        <PaperProvider>
             <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Box>
                     <Text>Open up App.js to start working on your app!</Text>
-                </Box>
             </SafeAreaView>
-        </GluestackUIProvider>
+        </PaperProvider>
     );
 }
