92718b4e1277e5badd13f286695ab668e07db226
Frankreed <baoanhtdn@gmail.com>
2024-01-18 16:41:33 -0800

    navigation to stats working

--- app/index.js
+++ app/index.js
@@ -3,14 +3,16 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { PaperProvider, Text } from 'react-native-paper';
 import {Link} from "expo-router";
 
-//This is the root file
 export default function Index() {
     return (
         <PaperProvider>
             <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                     <Text>Open up App.js to start working on your app!</Text>
-                    <Link href={"/drill/"}>/drill/</Link>
-                    <Link href={"/drill/1234"}>/drill/1234 slug</Link>
+                <Link href={{
+                    pathname: "/drills/stat"
+                }}>
+                    Go to Stats
+                </Link>
             </SafeAreaView>
         </PaperProvider>
     );
