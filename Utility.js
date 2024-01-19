a42e25f1e7a93387540ce3f5f97487c8fcc6f85b
Frankreed <baoanhtdn@gmail.com>
2024-01-18 16:41:33 -0800

    navigation to stats working

--- Utility.js
+++ Utility.js
@@ -1,15 +1,29 @@
-export const clampNumber = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
+export const clampNumber = (num, a, b) =>
+  Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
 export function formatDate(unixTimestamp) {
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const date = new Date(unixTimestamp * 1000);
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const date = new Date(unixTimestamp * 1000);
 
-    const year = date.getFullYear();
-    const month = months[date.getMonth()];
-    const day = date.getDate();
+  const year = date.getFullYear();
+  const month = months[date.getMonth()];
+  const day = date.getDate();
 
-    return `${month} ${day}, ${year}`;
+  return `${month} ${day}, ${year}`;
 }
 
-export function numTrunc(value){
-    return parseFloat(value.toFixed(3))
+export function numTrunc(value) {
+  return parseFloat(value.toFixed(3));
 }
