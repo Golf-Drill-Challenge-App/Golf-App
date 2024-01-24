12116eab7d9d9ce5deabcd56dd67f132d251885e
Jake Gehrke <gehrkej@oregonstate.edu>
2024-01-24 13:30:11 -0800

    Increase data to reflect a full 20 shots

--- app/drills/input/data/testData.js
+++ app/drills/input/data/testData.js
@@ -96,5 +96,389 @@ export const DrillData = {
         },
       ],
     },
+    {
+      shotNum: 5,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 74, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 6,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 99, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 7,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 104, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 8,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 9,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 10,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 11,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 12,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 13,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 14,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 15,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 16,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 17,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 18,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 19,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
+    {
+      shotNum: 20,
+      target: [
+        {
+          description: "Target Distance",
+          distanceMeasure: "yd",
+          value: 134, //this will need to be randomly generated
+        },
+      ],
+      inputs: [
+        {
+          id: "distance",
+          icon: "arrow-up",
+          prompt: "Carry Distance",
+          distanceMeasure: "yd",
+        },
+        {
+          id: "sideLanding",
+          icon: "arrow-left-right",
+          prompt: "Side Landing",
+          distanceMeasure: "ft",
+        },
+      ],
+    },
   ],
 };
