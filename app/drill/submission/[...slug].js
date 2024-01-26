24fa867c8db495d4f114e18d8375ae40033308c6
Frankreed <baoanhtdn@gmail.com>
2024-01-25 19:40:00 -0800

    Scroll list for shots works now!

--- app/drill/submission/[...slug].js
+++ app/drill/submission/[...slug].js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { PaperProvider, Text } from 'react-native-paper';
-import {Link} from "expo-router";
+import {Link, useLocalSearchParams} from "expo-router";
 
 export default function Index() {
+    const { slug } = useLocalSearchParams();
     return (
         <PaperProvider>
             <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text>Open up App.js to start working on your app!</Text>
-                    <Link href={"/drill/"}>/drill/</Link>
-                    <Link href={"/drill/1234"}>/drill/1234 slug</Link>
+                <Text>Submission Slug: {slug}</Text>
+
             </SafeAreaView>
         </PaperProvider>
     );
