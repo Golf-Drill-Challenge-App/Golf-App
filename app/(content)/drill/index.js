04d354c1d4846870876377e9dce9fd8c2885b163
Frank Nguyen <41023671+FrankreedX@users.noreply.github.com>
2024-01-27 15:56:04 -0800

    Laid out directory and files (#48)
    mapped out all pages

--- app/(content)/drill/index.js
+++ app/(content)/drill/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { PaperProvider, Text } from 'react-native-paper';
-import {Link} from "expo-router";
 
-//This is the root file
+//This is for the list of drills
 export default function Index() {
     return (
         <PaperProvider>
             <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text>Open up App.js to start working on your app!</Text>
-                    <Link href={"/drill/"}>/drill/</Link>
-                    <Link href={"/drill/1234"}>/drill/1234 slug</Link>
+                <Text>Drill Index</Text>
+
             </SafeAreaView>
         </PaperProvider>
     );
