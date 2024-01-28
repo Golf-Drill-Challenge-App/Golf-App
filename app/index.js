f0523f35b01e5056b246638569540fd840e0cbd2
Frankreed <baoanhtdn@gmail.com>
2024-01-27 16:16:49 -0800

    Moved all files into appropriate places. Added a babel plugin for root level imports

--- app/index.js
+++ app/index.js
@@ -9,9 +9,9 @@ export default function Index() {
             <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                     <Text>Open up App.js to start working on your app!</Text>
                 <Link href={{
-                    pathname: "/drills/stat"
+                    pathname: "/drill/1234/statistics"
                 }}>
-                    Go to Stats
+                    Go to Stats at /drill/1234/statistics
                 </Link>
             </SafeAreaView>
         </PaperProvider>
