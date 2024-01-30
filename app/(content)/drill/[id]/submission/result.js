aa8c2678e24f66c2946d033c8a539294c8425342
Frankreed <baoanhtdn@gmail.com>
2024-01-29 18:52:31 -0800

    Restart button

--- app/(content)/drill/[id]/submission/result.js
+++ app/(content)/drill/[id]/submission/result.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, Text, useWindowDimensions, View} from "react-native";
+import {Button, ScrollView, Text, useWindowDimensions, View} from "react-native";
 import ScatterChart from "react-native-scatter-chart";
 import ShotAccordion from "~/components/shotAccordion";
 
@@ -7,65 +7,68 @@ function Result(props) {
     const dots = props.submission.shots.map((value, index) => ([value["sideLanding"], value["carryDiff"]]))
     const {width} = useWindowDimensions()
     return (
-        <ScrollView>
-            <Text>Results</Text>
-            <View>
-                <Text>Strokes Gained</Text>
+        <>
+            <ScrollView>
+                <Text>Results</Text>
                 <View>
+                    <Text>Strokes Gained</Text>
                     <View>
-                        <Text>
-                            Total
-                        </Text>
-                        <Text>
-                            {props.submission["strokesGained"]}
-                        </Text>
-                    </View>
-                    <View>
-                        <Text>
-                            Average
-                        </Text>
-                        <Text>
-                            {props.submission["strokesGainedAverage"]}
-                        </Text>
+                        <View>
+                            <Text>
+                                Total
+                            </Text>
+                            <Text>
+                                {props.submission["strokesGained"]}
+                            </Text>
+                        </View>
+                        <View>
+                            <Text>
+                                Average
+                            </Text>
+                            <Text>
+                                {props.submission["strokesGainedAverage"]}
+                            </Text>
+                        </View>
                     </View>
                 </View>
-            </View>
-            <View>
-                <Text>Average Differences</Text>
                 <View>
-                    <Text>{props.submission["carryDiffAverage"]}</Text>
-                    <Text>{props.submission["sideLandingAverage"]}</Text>
-                    <Text>{props.submission["proxHoleAverage"]}</Text>
+                    <Text>Average Differences</Text>
+                    <View>
+                        <Text>{props.submission["carryDiffAverage"]}</Text>
+                        <Text>{props.submission["sideLandingAverage"]}</Text>
+                        <Text>{props.submission["proxHoleAverage"]}</Text>
+                    </View>
                 </View>
-            </View>
-            <View style={{
-                width: width,
-            }}>
-                <Text>Shot Tendency</Text>
-                <ScatterChart
-                    backgroundColor='#ffffff'
-                    data={[
-                        {
-                            color: 'blue',
-                            unit: '%',
-                            values: dots
-                        },
-                    ]}
-                    horizontalLinesAt={[0]}
-                    verticalLinesAt={[0]}
-                    minY={-10}
-                    maxY={10}
-                    minX={-35}
-                    maxX={35}
-                    chartWidth={width * 0.8}
-                />
-            </View>
+                <View style={{
+                    width: width,
+                }}>
+                    <Text>Shot Tendency</Text>
+                    <ScatterChart
+                        backgroundColor='#ffffff'
+                        data={[
+                            {
+                                color: 'blue',
+                                unit: '%',
+                                values: dots
+                            },
+                        ]}
+                        horizontalLinesAt={[0]}
+                        verticalLinesAt={[0]}
+                        minY={-10}
+                        maxY={10}
+                        minX={-35}
+                        maxX={35}
+                        chartWidth={width * 0.8}
+                    />
+                </View>
                 {props.submission["shots"].map(
                     (shot) =>
                         <ShotAccordion key={shot["sid"]} shot={shot} total={
                             props.submission["shots"].length}/>
                 )}
-        </ScrollView>
+            </ScrollView>
+            <Button title={"Restart"}/>
+        </>
     );
 }
 
