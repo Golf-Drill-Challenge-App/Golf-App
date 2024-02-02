42c4c1ff7832085e293f5e094e91dc191b2f8eb6
hannacol <hannacol@oregonstate.edu>
2024-02-01 19:07:19 -0800

    Drill description rebase

--- app/(content)/drill/[id]/index.js
+++ app/(content)/drill/[id]/index.js
@@ -1,15 +1,57 @@
 import React from 'react';
+import { View, StatusBar } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { PaperProvider, Text } from 'react-native-paper';
+import { PaperProvider, Text, SegmentedButtons, Appbar } from 'react-native-paper';
 
-//This is for the list of drills
+import Leaderboard from './drills/leaderboard'
+import Description from './drills/description'
+import Stat from './drills/stat'
+
 export default function Index() {
+    const [value, setValue] = React.useState("description");
+
+    const tabComponent = () => {
+        switch (value) {
+            case 'leaderboard':
+                return <Leaderboard />
+            case 'description':
+                return <Description />
+            case 'stats':
+                return <Stat />
+        }
+    }
+
     return (
         <PaperProvider>
-            <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Drill slugged</Text>
+            <Appbar.Header statusBarHeight={0}>
+                <Appbar.BackAction onPress={() => { }} color={"#F24E1E"} />
+                <Appbar.Content title="20 Shot Challenge" />
+            </Appbar.Header>
 
-            </SafeAreaView>
+            {/* Tab system */}
+
+            <SegmentedButtons
+                value={value}
+                onValueChange={setValue}
+                buttons={[
+                    {
+                        value: "description",
+                        label: "Description",
+                    },
+                    {
+                        value: "leaderboard",
+                        label: "Leaderboard",
+                    },
+                    {
+                        value: "stats",
+                        label: "Stats",
+                    },
+                ]}
+
+            />
+
+            {tabComponent()}
+
         </PaperProvider>
     );
 }
--- app/index.js
+++ app/index.js
@@ -1,57 +1,19 @@
 import React from 'react';
-import { View, StatusBar } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { PaperProvider, Text, SegmentedButtons, Appbar } from 'react-native-paper';
-
-import Leaderboard from './drills/leaderboard'
-import Description from './drills/description'
-import Stat from './drills/stat'
+import { PaperProvider, Text } from 'react-native-paper';
+import { Link } from "expo-router";
 
 export default function Index() {
-    const [value, setValue] = React.useState("description");
-
-    const tabComponent = () => {
-        switch (value) {
-            case 'leaderboard':
-                return <Leaderboard />
-            case 'description':
-                return <Description />
-            case 'stats':
-                return <Stat />
-        }
-    }
-
     return (
         <PaperProvider>
-            <Appbar.Header statusBarHeight={0}>
-                <Appbar.BackAction onPress={() => { }} color={"#F24E1E"} />
-                <Appbar.Content title="20 Shot Challenge" />
-            </Appbar.Header>
-
-            {/* Tab system */}
-
-            <SegmentedButtons
-                value={value}
-                onValueChange={setValue}
-                buttons={[
-                    {
-                        value: "description",
-                        label: "Description",
-                    },
-                    {
-                        value: "leaderboard",
-                        label: "Leaderboard",
-                    },
-                    {
-                        value: "stats",
-                        label: "Stats",
-                    },
-                ]}
-
-            />
-
-            {tabComponent()}
-
+            <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <Link href={{
+                    pathname: "/drill/1234/statistics"
+                }}>
+                    Go to Stats at /drill/1234/statistics
+                </Link>
+            </SafeAreaView>
         </PaperProvider>
     );
 }
