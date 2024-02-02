f2188f6bf3b64f1106aa4e3b95bdbbd341504d43
hannacol <91215417+hannacol@users.noreply.github.com>
2024-02-02 11:34:20 -0800

    Drilll Description merging into Layout (#53)
    * navigation to stats working
    
    * 2 dropdowns!
    
    * added dropdown, accordions, and drill submission view below the graph
    
    * Scroll list for shots works now!
    
    * Basic navigation for drill description, leaderboard and stats
    
    * disable header statusbar
    
    * fix: adjust height of status bar back button and screen title
    
    * navigation to stats working
    
    * Basic navigation for drill description, leaderboard and stats
    
    * disable header statusbar
    
    * fix: adjust height of status bar back button and screen title
    
    * Drill description rebase
    
    * Dynamic and parameratized routes for drill description + added drills.json
    
    ---------
    
    Co-authored-by: Frankreed <baoanhtdn@gmail.com>
    Co-authored-by: Artem Kolpakov <artemkolpakov78@gmail.com>

--- app/(content)/drill/index.js
+++ app/(content)/drill/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { PaperProvider, Text } from 'react-native-paper';
+import { List } from 'react-native-paper';
+import { Link } from 'expo-router';
+
+import drillsData from '~/drills.json'
 
 //This is for the list of drills
 export default function Index() {
+    const drills = drillsData.drills;
+
     return (
         <PaperProvider>
             <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text>Drill Index</Text>
+                <List.Section>
+                    <List.Subheader>Drills</List.Subheader>
+                    {drills.map((drill) => (
+                        <Link
+                            key={drill.did}
+                            href={{
+                                pathname: `/drill/${drill.did}`, 
+                                params: { id: drill.did },
+                            }}
+                            style={{ paddingVertical: 8 }}
+                        >
+                            <List.Item title={drill.drillType} description={drill.description} />
+                        </Link>
+                    ))}
+                </List.Section>
 
             </SafeAreaView>
         </PaperProvider>
