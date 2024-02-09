ae40b263b5f71de54fb999a377d5013ed3643ed8
Frankreed <baoanhtdn@gmail.com>
2024-02-08 21:28:40 -0800

    Made a profile page with a list of a user's attempted drills, and the corresponding drill stat page that respond to that drill's structure.

--- app/(content)/profile/index.js
+++ app/(content)/profile/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import drillData from "~/drill_data.json"
 import ProfileCard from "~/components/profileCard";
-import {Text} from "react-native";
+import {ScrollView, Text} from "react-native";
 import DrillCard from "~/components/drillCard";
 
 function Index(props) {
     const user = drillData["users"]["1"];
     const drills = drillData["drills"];
+    const attemptedDrills = Object.keys(drills).filter((drillId) => drillId in user["history"]);
     return (
-        <>
+        <ScrollView>
             <ProfileCard user={user} />
 
             <Text>Drill History</Text>
-            {Object.keys(drills).map((drillId) => {
+            {attemptedDrills.map((drillId) => {
+
                 return (
-                    <DrillCard drill={drills[drillId]} />
+                    <DrillCard drill={drills[drillId]} key={drillId}/>
                 );
             })}
-        </>
+        </ScrollView>
     );
 }
 
