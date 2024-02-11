7827deed579ca7290a68bd1166d287553dd6cb54
solderq35 <solderq35@gmail.com>
2024-02-10 16:38:47 -0800

    move to slot

--- app/(content)/drill/[id]/_layout.js
+++ app/(content)/drill/[id]/_layout.js
@@ -1,5 +1,5 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 
 export default function Layout() {
-  return <Stack  screenOptions={{ headerShown: false }} />;
+  return <Slot />;
 }
