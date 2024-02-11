929d3158681d6039a37ec682f7ee95882a9fb28c
solderq35 <solderq35@gmail.com>
2024-02-11 04:56:51 -0800

    update comments

--- app/_layout.js
+++ app/_layout.js
@@ -8,8 +8,8 @@ export default function AppLayout() {
     <Tabs initialRouteName="(content)" screenOptions={{ headerShown: false }}>
       <Tabs.Screen
         name="(content)/drill/index"
+        // show tab and tabbar
         options={{
-          // Ensure the tab always links to the same href.
           href: "/drill",
           title: "Drill",
           headerShown: false,
@@ -18,50 +18,50 @@ export default function AppLayout() {
 
       <Tabs.Screen
         name="(content)/drill/[id]/index"
+        // hide tab, show tabbar
         options={{
-          // Ensure the tab always links to the same href.
           href: null,
-          title: "Drill",
+          title: "Drill ID index",
           headerShown: false,
         }}
       />
 
       <Tabs.Screen
         name="(content)/drill/[id]/description"
+        // hide tab, show tabbar
         options={{
-          // Ensure the tab always links to the same href.
           href: null,
-          title: "Drill",
+          title: "Drill ID description",
           headerShown: false,
         }}
       />
 
       <Tabs.Screen
         name="(content)/drill/[id]/leaderboard"
+        // hide tab, show tabbar
         options={{
-          // Ensure the tab always links to the same href.
           href: null,
-          title: "Drill",
+          title: "Drill ID leaderboard",
           headerShown: false,
         }}
       />
 
       <Tabs.Screen
         name="(content)/drill/[id]/statistics"
+        // hide tab, show tabbar
         options={{
-          // Ensure the tab always links to the same href.
           href: null,
-          title: "Drill",
+          title: "Drill ID statistics",
           headerShown: false,
         }}
       />
 
       <Tabs.Screen
-        name="(content)/drill/[id]/submission"
+        name="(content)/drill/[id]/submission" // applies to all files in drill submission directory
+        // hide tab and tabbar
         options={{
-          // Ensure the tab always links to the same href.
           href: null,
-          title: "Drill",
+          title: "Drill submission directory",
           headerShown: false,
           tabBarStyle: {
             display: "none",
@@ -70,11 +70,11 @@ export default function AppLayout() {
       />
 
       <Tabs.Screen
-        name="(content)/drill/[id]/users"
+        name="(content)/drill/[id]/users" // this page comes up when you click on a specific user on a leaderboard
+        // hide tab and tabbar
         options={{
-          // Ensure the tab always links to the same href.
           href: null,
-          title: "Drill",
+          title: "Drill leaderboard user info",
           headerShown: false,
           tabBarStyle: {
             display: "none",
@@ -84,9 +84,9 @@ export default function AppLayout() {
 
       <Tabs.Screen
         name="(content)/team"
+        // show tab and tabbar
         options={{
-          // Ensure the tab always links to the same href.
-          href: "/team/",
+          href: "/team/", // only 1 tabscreen is needed due to _layout.js placement (like breadth-first search)
           title: "Team",
           headerShown: false,
         }}
@@ -94,26 +94,26 @@ export default function AppLayout() {
 
       <Tabs.Screen
         name="(content)/profile"
+        // show tab and tabbar
         options={{
-          // Ensure the tab always links to the same href.
-          href: "/profile/",
+          href: "/profile/", // only 1 tabscreen is needed due to _layout.js placement (like breadth-first search)
           title: "Profile",
           headerShown: false,
         }}
       />
       <Tabs.Screen
         name="(content)/plan"
+        // show tab and tabbar
         options={{
-          // Ensure the tab always links to the same href.
-          href: "/plan/",
+          href: "/plan/", // only 1 tabscreen is needed due to _layout.js placement (like breadth-first search)
           title: "Plan",
           headerShown: false,
         }}
       />
       <Tabs.Screen
+        // hide tab, show tabbar
         name="index"
         options={{
-          // Ensure the tab always links to the same href.
           href: null,
           headerShown: false,
         }}
