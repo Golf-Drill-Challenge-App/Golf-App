8911623ff0ae8955c9c036742e0c4f0846d6b829
solderq35 <solderq35@gmail.com>
2024-02-10 17:24:46 -0800

    add link back from drill submission index, remove extra layouts

--- app/(content)/drill/[id]/submission/index.js
+++ app/(content)/drill/[id]/submission/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { PaperProvider, Text } from 'react-native-paper';
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { PaperProvider, Text } from "react-native-paper";
+import { Link } from "expo-router";
 
 export default function Index() {
-    return (
-        <PaperProvider>
-            <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Submission</Text>
-
-            </SafeAreaView>
-        </PaperProvider>
-    );
+  return (
+    <PaperProvider>
+      <SafeAreaView
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <Link
+          href={{
+            pathname: "/",
+          }}
+        >
+          Go back to Index
+        </Link>
+        <Text>Submission</Text>
+      </SafeAreaView>
+    </PaperProvider>
+  );
 }
