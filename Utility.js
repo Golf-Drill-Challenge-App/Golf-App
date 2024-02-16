eab2a2cb8a3eb387c30293ec15dfbc5718f9b919
Frank Nguyen <41023671+FrankreedX@users.noreply.github.com>
2024-02-15 20:23:11 -0800

    Layouts/profile (#57)
    * sth
    
    * Made a profile page with a list of a user's attempted drills, and the corresponding drill stat page that respond to that drill's structure.
    
    * edited db_spec.jsonc to match drill_data.json
    
    * Made hitbox of statistics bigger.
    
    * componentized barchart!

--- Utility.js
+++ Utility.js
@@ -25,7 +25,7 @@ export function formatDate(unixTimestamp) {
 }
 
 export function numTrunc(value){
-    return parseFloat(value).toFixed(3);
+    return value === undefined ? "aaaaaaa" : parseFloat(value.toFixed(3))
 }
 
 export function calculateAverageProxToHole(drillSubmissions) {
