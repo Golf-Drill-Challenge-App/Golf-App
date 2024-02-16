adc2c03f8e946dcd7f80f14ac9ebf57aceba7a36
Jeff Huang <82061589+solderq35@users.noreply.github.com>
2024-02-15 20:12:04 -0800

    Layouts/teams (#59)
    * teams page init
    
    * don't hardcode pfp
    
    * switch to nested object data
    
    * object keys length
    
    * data for players on team
    
    * bottom sheet settings
    
    * set teamdata format like profile branch

---
+++ app/(content)/team/users/[user]/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import drillData from "~/team_data.json";
+import ProfileCard from "~/components/profileCard";
+import { ScrollView, Text } from "react-native";
+import DrillCard from "~/components/drillCard";
+import { Link, useLocalSearchParams } from "expo-router";
+
+function Index(props) {
+  const { user: user_id } = useLocalSearchParams();
+  const userData = drillData["users"][user_id];
+  const drills = drillData["drills"];
+  console.log(userData);
+  const attemptedDrills = Object.keys(drills).filter(
+    (drillId) => drillId in userData["history"]
+  );
+  // console.log(attemptedDrills);
+  return (
+    <ScrollView>
+      <Link
+        href={{
+          pathname: "/",
+        }}
+        style={{ marginBottom: 10 }}
+      >
+        Go back to Index
+      </Link>
+      <ProfileCard user={userData} />
+
+      <Text>Drill History</Text>
+      {attemptedDrills.map((drillId) => {
+        return (
+          <DrillCard
+            drill={drills[drillId]}
+            hrefString={"/team/users/" + userData.uid + "/drills/" + drillId}
+            key={drillId}
+          />
+        );
+      })}
+    </ScrollView>
+  );
+}
+
+export default Index;
