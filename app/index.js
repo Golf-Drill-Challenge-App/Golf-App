5c246f10636eb0d129dc0e6d6bed4b6add26c18e
hannacol <91215417+hannacol@users.noreply.github.com>
2024-02-15 19:56:50 -0800

    Layouts/drill leaderboard (#63)
    * Screen works!
    
    * Restart button
    
    * removed victory-native
    
    * feat: stylize drill submission results screen
    
    * feat: add rounding for all displayed data fields
    
    * fix: center scatter chart positioning
    
    * fix: stylize shot accordion width to fit Figma designs
    
    * Screen works!
    
    * Restart button
    
    * removed victory-native
    
    * feat: stylize drill submission results screen
    
    * feat: add rounding for all displayed data fields
    
    * fix: center scatter chart positioning
    
    * fix: stylize shot accordion width to fit Figma designs
    
    * not js
    
    * Updated drill.json and made leaderboards dynamic with json data
    
    * fix: revamp routing and navigation
    
    * feat: style drills list screen
    
    * feat: add icons for drill result screen
    
    Added icons to containers that display average differences data on the drill results screen, aligning screen's design with the Figma design. Ensured proper styling and centering of the data containers.
    
    * Refactored drill.json and leaderboard with new database structure
    
    * Drill results page loads with drill_data.json file
    
    ---------
    
    Co-authored-by: Frankreed <baoanhtdn@gmail.com>
    Co-authored-by: Artem Kolpakov <artemkolpakov78@gmail.com>

--- app/index.js
+++ app/index.js
@@ -1,44 +1,28 @@
-import React from "react";
-import { View, StatusBar } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { PaperProvider, Text } from "react-native-paper";
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { PaperProvider, Text } from 'react-native-paper';
 import { Link } from "expo-router";
-
 export default function Index() {
-  const [value, setValue] = React.useState("description");
-
-  const tabComponent = () => {
-    switch (value) {
-      case "leaderboard":
-        return <Leaderboard />;
-      case "description":
-        return <Description />;
-      case "stats":
-        return <Stat />;
-    }
-  };
-
-  return (
-    <PaperProvider>
-      <SafeAreaView
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      >
-        <Text>Open up App.js to start working on your app!</Text>
-        <Link
-          href={{
-            pathname: "/drills/stat",
-          }}
-        >
-          Go to Stats
-        </Link>
-        <Link
-          href={{
-            pathname: "/drill/732489/",
-          }}
-        >
-          Go to /drill/732489/
-        </Link>
-      </SafeAreaView>
-    </PaperProvider>
-  );
+    return (
+        <PaperProvider>
+            <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ alignItems: 'center' }}>Use the links below to test different screen prototypes:{"\n\n"}</Text>
+                <Link href={{
+                    pathname: "/drill"
+                }}>
+                    Go to Drills List
+                </Link>
+                <Link href={{
+                    pathname: "/drill/732489/statistics"
+                }}>
+                    Go to Stats at /drill/732489/statistics
+                </Link>
+                <Link href={{
+                    pathname: "/drill/732489/submission/"
+                }}>
+                    Go to Result at /drill/732489/submission/
+                </Link>
+            </SafeAreaView>
+        </PaperProvider>
+    );
 }
