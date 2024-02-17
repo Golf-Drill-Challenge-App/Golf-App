908ccbd168de75ce29862d61a92a30bbf9aa0858
Frankreed <baoanhtdn@gmail.com>
2024-02-16 19:56:14 -0800

    updated drill data generation script to push data to firestore

--- generate_drill_data.py
+++ generate_drill_data.py
@@ -1,6 +1,12 @@
 import random
 import time
 import math
+import firebase_admin
+from firebase_admin import firestore
+
+# Application Default credentials are automatically created.
+app = firebase_admin.initialize_app()
+db = firestore.client()
 
 def lookup_round_down(value, lookup_keys, lookup_values):
     if not lookup_keys or value < lookup_keys[0]:
@@ -17,6 +23,9 @@ sg_values = [2.25,2.25,2.3,2.35,2.4,2.45,2.5,2.55,2.6,2.625,2.65,2.68,2.7,2.713,
 
 putt_keys = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75]
 putt_values = [0,1.001,1.009,1.053,1.147,1.256,1.357,1.443,1.515,1.575,1.626,1.669,1.705,1.737,1.765,1.79,1.811,1.83,1.848,1.863,1.878,1.891,1.903,1.914,1.924,1.934,1.944,1.953,1.961,1.97,1.978,1.993,1.993,2.001,2.009,2.016,2.024,2.032,2.039,2.047,2.055,2.062,2.07,2.078,2.086,2.094,2.102,2.11,2.118,2.127,2.135,2.143,2.152,2.16,2.168,2.177,2.185,2.193,2.202,2.21,2.218,2.226,2.234,2.242,2.25,2.257,2.265,2.272,2.279,2.286,2.293,2.299,2.306,2.312,2.318,2.324]
+
+line_test = ["8", "9", "PW", "9", "8", "7", "8", "9", "PW", "9", "8", "7", "8", "9", "PW", "9", "8", "7", "8", "9"]
+
 
 # Function to generate random data for one submission
 def generate_submission(submission_id):
@@ -69,11 +78,42 @@ def generate_submission(submission_id):
         "proxHoleAverage": prox_hole_total / num_shots,
         "shots": shots
     }
+    db.collection("teams").document("1").collection("users").document("c0nEyjaOMhItMQTLMY0X").collection("SpvYyY94HaulVH2zmVyM").document(str(time_stamp)).set(submission)
+    return submission
+
+def generate_submission_line(submission_id):
+    # Unix timestamp for the submission
+    time_stamp = random.randint(1600000000, 1800000000)
+
+    # Strokes gained - random number between 1 and 10
+    side_landing_total = 0
+
+    # Generating 5 shots with random data
+    shots = []
+    for shot_id in range(1, len(line_test) + 1):
+        club = line_test[shot_id - 1]
+        side_landing = random.randint(-135, 135)
+        shot = {
+            "sid": shot_id,
+            "target": club,
+            "sideLanding": side_landing
+        }
+        side_landing_total += side_landing
+        shots.append(shot)
+
+    # One submission
+    submission = {
+        "time": time_stamp,
+        "sideLandingTotal": side_landing_total,
+        "sideLandingAverage": side_landing_total / len(shots),
+        "shots": shots
+    }
+    db.collection("teams").document("1").collection("users").document("c0nEyjaOMhItMQTLMY0X").collection("YtCsaxzscFScnpZYmnKI").document(str(time_stamp)).set(submission)
 
     return submission
 
 # Generate 100 submissions
-submissions = [generate_submission(i) for i in range(100)]
+submissions = [generate_submission_line(i) for i in range(100)]
 
 # Print the submissions without indentation or new lines
 print(submissions)
