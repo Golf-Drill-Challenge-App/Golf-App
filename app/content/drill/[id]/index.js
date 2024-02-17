e6edac3c2638acdffc82f9af26179cc7825e180a
Frankreed <baoanhtdn@gmail.com>
2024-02-16 18:05:29 -0800

    eradicated drills.json and unified everything under drill_data.json. Also made result.js use the mock data directly. Whoever is handling the input and result should resolve this

--- app/content/drill/[id]/index.js
+++ app/content/drill/[id]/index.js
@@ -1,114 +1,119 @@
-import React from 'react';
-import { PaperProvider, SegmentedButtons, Appbar } from 'react-native-paper';
-import { useNavigation, useLocalSearchParams } from 'expo-router';
+import React from "react";
+import { PaperProvider, SegmentedButtons, Appbar } from "react-native-paper";
+import { useNavigation, useLocalSearchParams } from "expo-router";
 
-import Leaderboard from './leaderboard'
-import Description from './description'
-import Stat from './statistics'
+import Leaderboard from "./leaderboard";
+import Description from "./description";
+import Stat from "./statistics";
 
-import drillsData from '~/drills.json'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import drillsData from "~/drill_data.json";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-    const [value, setValue] = React.useState("description");
-    const navigation = useNavigation();
-    const { id } = useLocalSearchParams();
-    const drillData = drillsData.teams["1"].drills[id] ? drillsData.teams["1"].drills[id] : null;
+  const [value, setValue] = React.useState("description");
+  const navigation = useNavigation();
+  const { id } = useLocalSearchParams();
+  const drillData = drillsData.teams["1"].drills[id]
+    ? drillsData.teams["1"].drills[id]
+    : null;
 
-    const findDrillAttempts = () => {
-        const drillAttempts = [];
-        const team = drillsData.teams["1"];
-        Object.values(team.users).forEach(user => {
-            Object.values(user.history).forEach(drill => {
-                if (Object.keys(user.history).includes(id)) {
-                    drill.forEach(attempt => {
-                        if (drillAttempts.length === 0) {
-                            drillAttempts.push({
-                                attempts: [
-                                    attempt.averageProximity,
-                                ],
-                                totalSubmissions: 1,
-                                userId: user.uid
-                            });
-                        }
-                        else {
-                            var userIdx = -1;
-                            for (let i = 0; i < drillAttempts.length; i++) {
-                                if (drillAttempts[i].userId === user.uid) {
-                                    userIdx = i;
-                                    break;
-                                }
-                            }
-
-                            if (userIdx >= 0) {
-                                drillAttempts[userIdx].attempts.push(attempt.averageProximity);
-                                drillAttempts[userIdx].totalSubmissions++;
-                            }
-                            else {
-                                drillAttempts.push({
-                                    attempts: [
-                                        attempt.averageProximity,
-                                    ],
-                                    totalSubmissions: 1,
-                                    userId: user.uid
-                                });
-                            }
-                        }
-                    })
+  const findDrillAttempts = () => {
+    const drillAttempts = [];
+    const team = drillsData.teams["1"];
+    Object.values(team.users).forEach((user) => {
+      Object.values(user.history).forEach((drill) => {
+        if (Object.keys(user.history).includes(id)) {
+          drill.forEach((attempt) => {
+            if (drillAttempts.length === 0) {
+              drillAttempts.push({
+                attempts: [attempt.averageProximity],
+                totalSubmissions: 1,
+                userId: user.uid,
+              });
+            } else {
+              var userIdx = -1;
+              for (let i = 0; i < drillAttempts.length; i++) {
+                if (drillAttempts[i].userId === user.uid) {
+                  userIdx = i;
+                  break;
                 }
-            })
-        });
+              }
 
-        return drillAttempts;
-    }
-
-    const drillLeaderboardAttempts = findDrillAttempts();
-
-    console.log("Attempts: ", drillLeaderboardAttempts)
-
-    const tabComponent = () => {
-        switch (value) {
-            case 'leaderboard':
-                return <Leaderboard leaderboardData={drillLeaderboardAttempts} drillId={id} />
-            case 'description':
-                return <Description descData={drillData} drillId={id} />
-            case 'stats':
-                return <Stat />
+              if (userIdx >= 0) {
+                drillAttempts[userIdx].attempts.push(attempt.averageProximity);
+                drillAttempts[userIdx].totalSubmissions++;
+              } else {
+                drillAttempts.push({
+                  attempts: [attempt.averageProximity],
+                  totalSubmissions: 1,
+                  userId: user.uid,
+                });
+              }
+            }
+          });
         }
+      });
+    });
+
+    return drillAttempts;
+  };
+
+  const drillLeaderboardAttempts = findDrillAttempts();
+
+  console.log("Attempts: ", drillLeaderboardAttempts);
+
+  const tabComponent = () => {
+    switch (value) {
+      case "leaderboard":
+        return (
+          <Leaderboard
+            leaderboardData={drillLeaderboardAttempts}
+            drillId={id}
+          />
+        );
+      case "description":
+        return <Description descData={drillData} drillId={id} />;
+      case "stats":
+        return <Stat />;
     }
+  };
 
-    return (
-        <PaperProvider>
-            <SafeAreaView>
-                <Appbar.Header statusBarHeight={0} style={{ backgroundColor: "FFF" }}>
-                    <Appbar.BackAction onPress={() => { navigation.goBack() }} color={"#F24E1E"} />
-                    <Appbar.Content title={drillData.drillType} />
-                </Appbar.Header>
+  return (
+    <PaperProvider>
+      <SafeAreaView>
+        <Appbar.Header statusBarHeight={0} style={{ backgroundColor: "FFF" }}>
+          <Appbar.BackAction
+            onPress={() => {
+              navigation.goBack();
+            }}
+            color={"#F24E1E"}
+          />
+          <Appbar.Content title={drillData.drillType} />
+        </Appbar.Header>
 
-                {/* Tab system */}
+        {/* Tab system */}
 
-                <SegmentedButtons
-                    value={value}
-                    onValueChange={setValue}
-                    buttons={[
-                        {
-                            value: "description",
-                            label: "Description",
-                        },
-                        {
-                            value: "leaderboard",
-                            label: "Leaderboard",
-                        },
-                        {
-                            value: "stats",
-                            label: "Stats",
-                        },
-                    ]}
+        <SegmentedButtons
+          value={value}
+          onValueChange={setValue}
+          buttons={[
+            {
+              value: "description",
+              label: "Description",
+            },
+            {
+              value: "leaderboard",
+              label: "Leaderboard",
+            },
+            {
+              value: "stats",
+              label: "Stats",
+            },
+          ]}
+        />
 
-                />
-
-                {tabComponent()}
-            </SafeAreaView>
-        </PaperProvider>
-    );
+        {tabComponent()}
+      </SafeAreaView>
+    </PaperProvider>
+  );
 }
--- app/content/drill/[id]/leaderboard.js
+++ app/content/drill/[id]/leaderboard.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import { View } from 'react-native'
-import { List, Text, Icon, Avatar } from 'react-native-paper'
+import React from "react";
+import { View } from "react-native";
+import { List, Text, Icon, Avatar } from "react-native-paper";
 
-import { calculateAverageProxToHole, takeBestScore } from '~/Utility'
-import drillsData from '~/drills.json'
+import { calculateAverageProxToHole, takeBestScore } from "~/Utility";
+import drillsData from "~/drill_data.json";
 
 export default function Leaderboard({ leaderboardData, drillId }) {
-    const bestScores = takeBestScore(leaderboardData);
-    const orderedLeaderboard = bestScores.sort((a, b) => a.score - b.score)
+  const bestScores = takeBestScore(leaderboardData);
+  const orderedLeaderboard = bestScores.sort((a, b) => a.score - b.score);
 
-    const getUserInfo = (userId) => {
-        return drillsData.teams["1"].users[userId];
-    }
+  const getUserInfo = (userId) => {
+    return drillsData.teams["1"].users[userId];
+  };
 
-    return (
-        <List.Section>
-            {orderedLeaderboard.map((user) => (
-                <List.Item
-                    key={user.user}
-                    title={getUserInfo(user.user).name}
-                    left={() => <Avatar.Text size={24} label="XD" />}
-                    right={() => (
-                        <View style={{ flexDirection: "row", alignItems: "center" }}>
-                            <Text>{user.score} ft</Text>
-                            <Icon source="chevron-right" />
-                        </View>
-                    )}
-                />
-            ))}
-        </List.Section >
-    )
+  return (
+    <List.Section>
+      {orderedLeaderboard.map((user) => (
+        <List.Item
+          key={user.user}
+          title={getUserInfo(user.user).name}
+          left={() => <Avatar.Text size={24} label="XD" />}
+          right={() => (
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Text>{user.score} ft</Text>
+              <Icon source="chevron-right" />
+            </View>
+          )}
+        />
+      ))}
+    </List.Section>
+  );
 }
