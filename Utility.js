6ec5739321112e55641e255046c58c20e3be7436
Frank Nguyen <41023671+FrankreedX@users.noreply.github.com>
2024-02-19 17:24:22 -0800

    Frankreed/incorporating prettier (#73)
    * mass formatting
    
    * added easy npm command to start prettier
    
    * mass reformat 2

--- Utility.js
+++ Utility.js
@@ -1,5 +1,6 @@
 export const clampNumber = (num, a, b) =>
   Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
+
 export function formatDate(unixTimestamp) {
   const months = [
     "Jan",
@@ -24,76 +25,73 @@ export function formatDate(unixTimestamp) {
   return `${month} ${day}, ${year}`;
 }
 
-export function numTrunc(value){
-    return value === undefined ? "aaaaaaa" : parseFloat(value.toFixed(3))
+export function numTrunc(value) {
+  return value === undefined ? "aaaaaaa" : parseFloat(value.toFixed(3));
 }
 
 export function calculateAverageProxToHole(drillSubmissions) {
-    const userAverages = [];
-    drillSubmissions.forEach(submission => {
-        const userId = submission.userId;
-        let totalProximity = 0;
-        submission.shots.forEach(shot => {
-            const proximity = Math.sqrt(Math.pow(Math.abs(shot.distance - shot.target) * 3, 2) + Math.pow(shot.sideLanding, 2));
-            totalProximity += proximity;
-        });
-        const averageProximity = Math.round((totalProximity / submission.shots.length) * 10) / 10;
-
-        if (userAverages.length === 0) {
-            userAverages.push({
-                attempts: [
-                    averageProximity,
-                ],
-                totalSubmissions: 1,
-                userId: userId
-            });
-        }
-        else {
-            var userIdx = -1;
-            for (let i = 0; i < userAverages.length; i++) {
-                if (userAverages[i].userId === userId) {
-                    userIdx = i;
-                    break;
-                }
-            }
-
-            if (userIdx >= 0) {
-                userAverages[userIdx].attempts.push(averageProximity);
-                userAverages[userIdx].totalSubmissions++;
-            }
-            else {
-                userAverages.push({
-                    attempts: [
-                        averageProximity,
-                    ],
-                    totalSubmissions: 1,
-                    userId: userId
-                });
-            }
-        }
+  const userAverages = [];
+  drillSubmissions.forEach((submission) => {
+    const userId = submission.userId;
+    let totalProximity = 0;
+    submission.shots.forEach((shot) => {
+      const proximity = Math.sqrt(
+        Math.pow(Math.abs(shot.distance - shot.target) * 3, 2) +
+          Math.pow(shot.sideLanding, 2),
+      );
+      totalProximity += proximity;
     });
+    const averageProximity =
+      Math.round((totalProximity / submission.shots.length) * 10) / 10;
 
-    return userAverages;
+    if (userAverages.length === 0) {
+      userAverages.push({
+        attempts: [averageProximity],
+        totalSubmissions: 1,
+        userId: userId,
+      });
+    } else {
+      var userIdx = -1;
+      for (let i = 0; i < userAverages.length; i++) {
+        if (userAverages[i].userId === userId) {
+          userIdx = i;
+          break;
+        }
+      }
+
+      if (userIdx >= 0) {
+        userAverages[userIdx].attempts.push(averageProximity);
+        userAverages[userIdx].totalSubmissions++;
+      } else {
+        userAverages.push({
+          attempts: [averageProximity],
+          totalSubmissions: 1,
+          userId: userId,
+        });
+      }
+    }
+  });
+
+  return userAverages;
 }
 
 export function takeBestScore(userAverages) {
-    const bestSubmissions = [];
-    userAverages.forEach(user => {
-        var bestScore = Number.MAX_VALUE;
-        user.attempts.forEach(attempt => {
-            if (attempt < bestScore) {
-                bestScore = attempt;
-            };
-        });
-        bestSubmissions.push({
-            user: user.userId,
-            score: bestScore
-        });
+  const bestSubmissions = [];
+  userAverages.forEach((user) => {
+    var bestScore = Number.MAX_VALUE;
+    user.attempts.forEach((attempt) => {
+      if (attempt < bestScore) {
+        bestScore = attempt;
+      }
     });
+    bestSubmissions.push({
+      user: user.userId,
+      score: bestScore,
+    });
+  });
 
-    return bestSubmissions;
+  return bestSubmissions;
 }
-
 
 export function createOutputData(inputValues, attemptData) {
   //Construct an array of each shot with keys, inputValues, and target Distances
