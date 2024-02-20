1928b91567e96106460488defd3890e935613c67
Jeff Huang <82061589+solderq35@users.noreply.github.com>
2024-02-19 17:20:47 -0800

    move drill submission files to new directory to hide tab bar (#68)
    * move drill submission to new directory to hide bottom nav
    
    * eradicated drills.json and unified everything under drill_data.json. Also made result.js use the mock data directly. Whoever is handling the input and result should resolve this
    
    * fix data imports for profile, teams, drill stats
    
    ---------
    
    Co-authored-by: Frankreed <baoanhtdn@gmail.com>

--- app/content/team/users/[user]/drills/[id].js
+++ app/content/team/users/[user]/drills/[id].js
@@ -6,13 +6,13 @@ import BarChartScreen from "~/components/barChart";
 
 export default function Stat() {
   const { user: user_id, id: drill_id } = useLocalSearchParams();
-  console.log(drillData.users[user_id]);
-  console.log(drillData.drills[drill_id]);
+  console.log(drillData["teams"]["1"].users[user_id]);
+  console.log(drillData["teams"]["1"].drills[drill_id]);
   return (
     <>
       <BarChartScreen
-        drillData={drillData["users"][user_id]["history"][drill_id]}
-        mainOutputAttempt={drillData["drills"][drill_id]["mainOutputAttempt"]}
+        drillData={drillData["teams"]["1"]["users"][user_id]["history"][drill_id]}
+        mainOutputAttempt={drillData["teams"]["1"]["drills"][drill_id]["mainOutputAttempt"]}
       />
     </>
   );
