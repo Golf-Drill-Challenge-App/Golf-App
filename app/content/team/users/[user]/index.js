1617b7b93ea100327a57ed4fb65784e78f5929d4
Artem Kolpakov <artemkolpakov78@gmail.com>
2024-02-20 23:38:49 -0800

    feat: style the profile card component

--- app/content/team/users/[user]/index.js
+++ app/content/team/users/[user]/index.js
@@ -1,48 +1,68 @@
-import { Link, useLocalSearchParams } from "expo-router";
-import { ScrollView, Text } from "react-native";
-import DrillCard from "~/components/drillCard";
-import ProfileCard from "~/components/profileCard";
+import React from "react";
 import drillData from "~/drill_data.json";
+import ProfileCard from "~/components/profileCard";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import DrillCard from "~/components/drillCard";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { PaperProvider, Appbar } from "react-native-paper";
 
 function Index(props) {
   const { user: user_id } = useLocalSearchParams();
   const userData = drillData["teams"]["1"]["users"][user_id];
   const drills = drillData["teams"]["1"]["drills"];
+  const navigation = useNavigation();
   console.log(userData);
   const attemptedDrills = userData["history"]
     ? Object.keys(drills).filter((drillId) => drillId in userData["history"])
     : [];
-  // console.log(attemptedDrills);
-  return (
-    <ScrollView>
-      <Link
-        href={{
-          pathname: "/",
-        }}
-        style={{ marginBottom: 10 }}
-      >
-        Go back to Index
-      </Link>
-      <ProfileCard user={userData} />
 
-      <Text>Drill History</Text>
-      {userData["history"] ? (
-        attemptedDrills.map((drillId) => {
-          return (
-            <DrillCard
-              drill={drills[drillId]}
-              hrefString={
-                "/content/team/users/" + userData.uid + "/drills/" + drillId
-              }
-              key={drillId}
-            />
-          );
-        })
-      ) : (
-        <Text>No drills attempted yet</Text>
-      )}
-    </ScrollView>
+  return (
+    <PaperProvider>
+      <SafeAreaView>
+        <Appbar.Header statusBarHeight={0} style={{ backgroundColor: "FFF" }}>
+          <Appbar.BackAction
+            onPress={() => {
+              navigation.goBack();
+            }}
+            color={"#F24E1E"}
+          />
+          <Appbar.Content title={""} />
+        </Appbar.Header>
+
+        <View style={styles.profileContainer}>
+          <ProfileCard user={userData} />
+        </View>
+
+        <Text>Drills</Text>
+
+        <ScrollView>
+          {userData["history"] ? (
+            attemptedDrills.map((drillId) => {
+              return (
+                <DrillCard
+                  drill={drills[drillId]}
+                  hrefString={
+                    "/content/team/users/" + userData.uid + "/drills/" + drillId
+                  }
+                  key={drillId}
+                />
+              );
+            })
+          ) : (
+            <Text>No drills attempted yet</Text>
+          )}
+        </ScrollView>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+});
 
 export default Index;
