15fdee633fe15749a8025845f4137bc459e14ae9
Frankreed <baoanhtdn@gmail.com>
2024-02-22 15:13:25 -0800

    Changed database so single attempt screen can more easily display for multiple drills

--- app/content/drill/[id]/attempts/[attempt].js
+++ app/content/drill/[id]/attempts/[attempt].js
@@ -8,7 +8,7 @@ import {
   View,
   useWindowDimensions,
 } from "react-native";
-import { Icon } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 import ScatterChart from "react-native-scatter-chart";
 import { numTrunc } from "~/Utility";
 import ShotAccordion from "~/components/shotAccordion";
@@ -17,11 +17,13 @@ import { db } from "~/firebaseConfig";
 function Result() {
   const drillId = useLocalSearchParams()["id"];
   const attemptId = useLocalSearchParams()["attempt"];
-  const [drillInfo, setDrillInfo] = useState({ outputs: [] });
+  const [drillInfo, setDrillInfo] = useState({});
   const [attempt, setAttempt] = useState({});
 
   let dots = [];
   if (
+    Object.keys(drillInfo).length > 0 &&
+    Object.keys(attempt).length > 0 &&
     drillInfo["outputs"].includes("sideLanding") &&
     drillInfo["outputs"].includes("carryDiff")
   ) {
@@ -56,83 +58,100 @@ function Result() {
 
   return (
     <>
-      <ScrollView contentContainerStyle={styles.container}>
-        <Text style={styles.sectionTitle}>Drill Results</Text>
+      <SafeAreaView>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Text style={styles.sectionTitle}>Drill Results</Text>
 
-        <View style={styles.dataSection}>
-          <Text style={styles.dataTitle}>Strokes Gained</Text>
-          <View style={styles.dataRow}>
-            <Text style={styles.dataLabel}>Total: </Text>
-            <Text style={styles.dataValue}>
-              {numTrunc(attempt["strokesGained"])}
-            </Text>
-          </View>
-          <View style={styles.dataRow}>
-            <Text style={styles.dataLabel}>Average: </Text>
-            <Text style={styles.dataValue}>
-              {numTrunc(attempt["strokesGainedAverage"])}
-            </Text>
-          </View>
-        </View>
+          {Object.keys(drillInfo).length > 0 &&
+            drillInfo["aggOutputs"].map((output) => (
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+                key={output}
+              >
+                <Text>{output}</Text>
+                <Text>{numTrunc(attempt[output])}</Text>
+              </View>
+            ))}
 
-        <View style={styles.dataSection}>
-          <Text style={styles.dataTitle}>Average Differences</Text>
-          <View style={styles.dataRow}>
-            <Icon source={"arrow-up-down"} />
-            <Text style={styles.dataValue}>
-              {numTrunc(attempt["carryDiffAverage"])}
-            </Text>
-          </View>
-          <View style={styles.dataRow}>
-            <Icon source={"arrow-left-right"} />
-            <Text style={styles.dataValue}>
-              {numTrunc(attempt["sideLandingAverage"])}
-            </Text>
-          </View>
-          <View style={styles.dataRow}>
-            <Icon source={"flag"} />
-            <Text style={styles.dataValue}>
-              {numTrunc(attempt["proxHoleAverage"])}
-            </Text>
-          </View>
-        </View>
+          {/*<View style={styles.dataSection}>*/}
+          {/*  <Text style={styles.dataTitle}>Strokes Gained</Text>*/}
+          {/*  <View style={styles.dataRow}>*/}
+          {/*    <Text style={styles.dataLabel}>Total: </Text>*/}
+          {/*    <Text style={styles.dataValue}>*/}
+          {/*      {numTrunc(attempt["strokesGained"])}*/}
+          {/*    </Text>*/}
+          {/*  </View>*/}
+          {/*  <View style={styles.dataRow}>*/}
+          {/*    <Text style={styles.dataLabel}>Average: </Text>*/}
+          {/*    <Text style={styles.dataValue}>*/}
+          {/*      {numTrunc(attempt["strokesGainedAverage"])}*/}
+          {/*    </Text>*/}
+          {/*  </View>*/}
+          {/*</View>*/}
 
-        {dots.length > 0 && (
-          <View style={styles.chartSection}>
-            <Text style={styles.sectionTitle}>Shot Tendency</Text>
-            <View style={{ ...styles.chartContainer, width: width * 0.8 }}>
-              <ScatterChart
-                style={styles.chart}
-                backgroundColor="#ffffff"
-                data={[
-                  {
-                    color: "blue",
-                    unit: "%",
-                    values: dots,
-                  },
-                ]}
-                horizontalLinesAt={[0]}
-                verticalLinesAt={[0]}
-                minY={-10}
-                maxY={10}
-                minX={-35}
-                maxX={35}
-                chartWidth={width * 0.8}
-              />
+          {/*<View style={styles.dataSection}>*/}
+          {/*  <Text style={styles.dataTitle}>Average Differences</Text>*/}
+          {/*  <View style={styles.dataRow}>*/}
+          {/*    <Icon source={"arrow-up-down"} />*/}
+          {/*    <Text style={styles.dataValue}>*/}
+          {/*      {numTrunc(attempt["carryDiffAverage"])}*/}
+          {/*    </Text>*/}
+          {/*  </View>*/}
+          {/*  <View style={styles.dataRow}>*/}
+          {/*    <Icon source={"arrow-left-right"} />*/}
+          {/*    <Text style={styles.dataValue}>*/}
+          {/*      {numTrunc(attempt["sideLandingAverage"])}*/}
+          {/*    </Text>*/}
+          {/*  </View>*/}
+          {/*  <View style={styles.dataRow}>*/}
+          {/*    <Icon source={"flag"} />*/}
+          {/*    <Text style={styles.dataValue}>*/}
+          {/*      {numTrunc(attempt["proxHoleAverage"])}*/}
+          {/*    </Text>*/}
+          {/*  </View>*/}
+          {/*</View>*/}
+
+          {dots.length > 0 && (
+            <View style={styles.chartSection}>
+              <Text style={styles.sectionTitle}>Shot Tendency</Text>
+              <View style={{ ...styles.chartContainer, width: width * 0.8 }}>
+                <ScatterChart
+                  style={styles.chart}
+                  backgroundColor="#ffffff"
+                  data={[
+                    {
+                      color: "blue",
+                      unit: "%",
+                      values: dots,
+                    },
+                  ]}
+                  horizontalLinesAt={[0]}
+                  verticalLinesAt={[0]}
+                  minY={-10}
+                  maxY={10}
+                  minX={-35}
+                  maxX={35}
+                  chartWidth={width * 0.8}
+                />
+              </View>
             </View>
-          </View>
-        )}
+          )}
 
-        {attempt["shots"] &&
-          attempt["shots"].map((shot) => (
-            <ShotAccordion
-              key={shot["sid"]}
-              shot={shot}
-              drillInfo={drillInfo}
-              total={numTrunc(attempt["shots"].length)}
-            />
-          ))}
-      </ScrollView>
+          {attempt["shots"] &&
+            attempt["shots"].map((shot) => (
+              <ShotAccordion
+                key={shot["sid"]}
+                shot={shot}
+                drillInfo={drillInfo}
+                total={numTrunc(attempt["shots"].length)}
+              />
+            ))}
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 }
