55d9645c449c69d119fee532bf3a359681b09dd9
Jake Gehrke <gehrkej@oregonstate.edu>
2024-02-21 19:11:37 -0800

    added export to functions

--- Utility.js
+++ Utility.js
@@ -107,7 +107,7 @@ export function takeBestScore(userAverages) {
 
   return bestSubmissions;
 }
-function lookUpBaselineStrokesGained(value) {
+export function lookUpBaselineStrokesGained(value) {
   const sgKeys = [
     0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90,
     95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165,
@@ -126,7 +126,7 @@ function lookUpBaselineStrokesGained(value) {
     }
   }
 }
-function lookUpExpectedPutts(value) {
+export function lookUpExpectedPutts(value) {
   const putt_keys = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
