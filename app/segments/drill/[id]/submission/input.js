c4df16dfa05948bfbd4e26673d805618173914da
Jake Gehrke <gehrkej@oregonstate.edu>
2024-02-22 19:24:14 -0800

    passing outputData into input for database uploading

--- app/segments/drill/[id]/submission/input.js
+++ app/segments/drill/[id]/submission/input.js
@@ -167,7 +167,12 @@ function createOutputData(inputValues, attemptData) {
   };
 }
 
-export default function Input({ attemptData, setToggleResult, setOutputData }) {
+export default function Input({
+  outputData,
+  attemptData,
+  setToggleResult,
+  setOutputData,
+}) {
   //Helper varibles
   const numInputs = attemptData.inputs.length;
 
