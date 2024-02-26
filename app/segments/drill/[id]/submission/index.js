fa307316f0ab5739aeea1949d8fd1354e60a438a
Frank Nguyen <41023671+FrankreedX@users.noreply.github.com>
2024-02-25 18:51:38 -0800

    Make drill pages (list, desc, leaderboard, and stat) fetch data from database (#74)
    * move drill submission to new directory to hide bottom nav
    
    * eradicated drills.json and unified everything under drill_data.json. Also made result.js use the mock data directly. Whoever is handling the input and result should resolve this
    
    * mass formatting
    
    * drill list page is now based on firebase
    
    * updated db_spec
    
    * checkpoint save. moved drill attempts into their own thing at the root of teams.
    
    * *very* ugly implementation of drill list and drill leaderboard off of firestore
    
    * make stuff work
    
    * description, leaderboard, and statistics all use firebase now.
    
    * leaderboard attempts can be navigated into. The resulting page only works with 20 shot challenge
    
    * mass reformat, again!
    
    * Changed database so single attempt screen can more easily display for multiple drills
    
    * added some safe area, consolidated shotAccordion.js
    
    ---------
    
    Co-authored-by: solderq35 <solderq35@gmail.com>

--- app/segments/drill/[id]/submission/index.js
+++ app/segments/drill/[id]/submission/index.js
@@ -1,73 +1,19 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { PaperProvider } from "react-native-paper";
-
+import { AttemptData } from "~/testData";
 import Input from "./input";
-import Result from "./result";
-import { lookUpBaselineStrokesGained } from "~/Utility";
 
 export default function Index() {
-  const attemptData = {
-    requirements: [
-      {
-        id: "distance",
-        description: "Target Distance",
-        distanceMeasure: "yd",
-      },
-    ],
-    inputs: [
-      {
-        id: "carry",
-        icon: "arrow-up",
-        prompt: "Carry Distance",
-        distanceMeasure: "yd",
-      },
-      {
-        id: "sideLanding",
-        icon: "arrow-left-right",
-        prompt: "Side Landing",
-        distanceMeasure: "ft",
-      },
-    ],
-  };
-  const fillShotTargets = (min, max) => {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    let shots = [];
+  //Franks thoughts: State should be shared here between
 
-    for (var i = 0; i < 20; i++) {
-      var target = Math.floor(
-        Math.random() * (maxFloored - minCeiled + 1) + minCeiled,
-      );
-      var baseline = lookUpBaselineStrokesGained(target);
-      shots.push({
-        shotNum: i + 1,
-        value: target,
-        baseline: baseline,
-      });
-    }
-    console.log("Attempt Data changed");
-    return shots;
-  };
+  //a useState hook to track the inputs on each shot
+  const [inputValues, setInputValues] = useState(
+    Array.from({ length: AttemptData.shots.length }, () => ({})),
+  );
 
-  attemptData.shots = fillShotTargets(100, 150); //current this is getting recalled everytime state changes
-
-  const [outputData, setOutputData] = useState([]);
-  const [toggleResult, setToggleResult] = useState(false);
-
-  const display = () => {
-    if (toggleResult == true) {
-      return <Result submission={outputData} />;
-    } else {
-      return (
-        <Input
-          outputData={outputData}
-          attemptData={attemptData}
-          setToggleResult={setToggleResult}
-          setOutputData={setOutputData}
-        />
-      );
-    }
-  };
-
-  return <PaperProvider>{display()}</PaperProvider>;
+  return (
+    <PaperProvider>
+      <Input inputValues={inputValues} setInputValues={setInputValues} />
+    </PaperProvider>
+  );
 }
