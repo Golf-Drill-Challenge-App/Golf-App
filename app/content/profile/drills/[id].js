0eb3b216d1955d1b466f94f6f1307e34013ce87c
Artem <91341345+artkolpakov@users.noreply.github.com>
2024-02-25 19:20:20 -0800

    Style Profile and Statistics Screens (#77)
    * feat: style the profile card component
    
    * feat: style the drill card component
    
    * feat: style the personal and user profile pages
    
    * feat: style the statistics page
    
    * fix: adjust barChart component styling
    
    * fix: add leaderboard margins
    
    * feat: style the ShotAccordion component
    
    * feat: style the data field rows within the ShotAccordion component
    
    * fixed bar charts and stuff
    
    ---------
    
    Co-authored-by: Frankreed <baoanhtdn@gmail.com>

--- app/content/profile/drills/[id].js
+++ app/content/profile/drills/[id].js
@@ -13,6 +13,7 @@ import { Appbar, PaperProvider } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BarChartScreen from "~/components/barChart";
 import { db } from "~/firebaseConfig";
+import { PaperProvider, Appbar } from "react-native-paper";
 
 export default function Stat() {
   const drillId = useLocalSearchParams()["id"];
