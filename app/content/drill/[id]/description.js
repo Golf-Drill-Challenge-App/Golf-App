33e9a78c1cd02c2f9dd93a64d77f52829b55a1d9
hannacol <hannacol@oregonstate.edu>
2024-02-26 17:26:31 -0800

    Images now have a modal popup when pressed and close when anywhere is pressed

--- app/content/drill/[id]/description.js
+++ app/content/drill/[id]/description.js
@@ -1,19 +1,40 @@
-import { Link, useLocalSearchParams } from "expo-router";
-import { Image, View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { useState } from "react";
+import { Image, View, Pressable, TouchableWithoutFeedback } from "react-native";
+import { Button, Text, Portal, Modal } from "react-native-paper";
+import { Link } from "expo-router";
 
-export default function Description({ descData }) {
-  const drillId = useLocalSearchParams()["id"];
+export default function Description({ descData, drillId }) {
+    const [visible, setVisible] = useState(false);
+
+    const showModal = () => setVisible(true);
+    const hideModal = () => setVisible(false);
+
   return (
     <View style={{ margin: 10 }}>
+      <Portal>
+        <Modal visible={visible} onDismiss={hideModal} style={{ margin: 10 }}>
+          <TouchableWithoutFeedback onPress={hideModal}>
+            <View style={{ justifyContent: "center", height: "100%", width: "100%" }} >
+              <Image
+               source={require("~/assets/drill-description-image.jpg")}
+               style={{ width: "100%", maxHeight: 200 }}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+        </Modal>
+      </Portal>
       <Text style={{ paddingBottom: 10 }} variant="headlineLarge">
         Description
       </Text>
       <Text variant="bodySmall">{descData.description}</Text>
-      <Image
-        source={require("~/assets/drill-description-image.jpg")}
-        style={{ width: "100%", maxHeight: 200, marginTop: 50 }}
-      />
+      <View style={{ marginTop: 10 }}>
+        <Pressable onPress={showModal} style={{ backgroundColor: "red", height: 200 }}>
+          <Image
+           source={require("~/assets/drill-description-image.jpg")}
+           style={{ width: "100%", maxHeight: 200 }}
+          />
+        </Pressable>
+      </View>
       <Link
         href={{
           pathname: `/segments/drill/${drillId}/submission`,
