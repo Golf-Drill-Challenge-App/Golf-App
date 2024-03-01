1c1d1f46ce4a6027238e00fe22717af06b084377
hannacol <hannacol@oregonstate.edu>
2024-03-01 11:57:32 -0800

    Changed AttemptData requirments to use database

--- app/segments/drill/[id]/submission/index.js
+++ app/segments/drill/[id]/submission/index.js
@@ -1,19 +1,105 @@
-import { useState } from "react";
-import { PaperProvider } from "react-native-paper";
-import { AttemptData } from "~/testData";
+import { useLocalSearchParams } from "expo-router";
+import React, { useState, useEffect } from "react";
+import { PaperProvider, Text } from "react-native-paper";
+
 import Input from "./input";
+import Result from "./result";
+import { lookUpBaselineStrokesGained } from "~/Utility";
+
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "~/firebaseConfig";
 
 export default function Index() {
-  //Franks thoughts: State should be shared here between
+  const { id } = useLocalSearchParams();
 
-  //a useState hook to track the inputs on each shot
-  const [inputValues, setInputValues] = useState(
-    Array.from({ length: AttemptData.shots.length }, () => ({})),
-  );
+  const drillsRef = doc(db, "teams", "1", "drills", id);
 
-  return (
-    <PaperProvider>
-      <Input inputValues={inputValues} setInputValues={setInputValues} />
-    </PaperProvider>
-  );
+  const [drillData, setDrillData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+      const fetchData = async () => {
+          try {
+              setLoading(true);
+              await getDoc(drillsRef).then((document) => {
+                  setDrillData(document.data());
+              });
+
+              setLoading(false);
+          }
+          catch (error) {
+              console.error("Could not fetch data from database.", error);
+              setLoading(false);
+          }
+      }
+      fetchData();
+    
+  }, []);
+
+
+  const attemptData = {
+    requirements: drillData.requirements,
+    inputs: [
+      {
+        id: "carry",
+        icon: "arrow-up",
+        prompt: "Carry Distance",
+        distanceMeasure: "yd",
+      },
+      {
+        id: "sideLanding",
+        icon: "arrow-left-right",
+        prompt: "Side Landing",
+        distanceMeasure: "ft",
+      },
+    ],
+  };
+  const fillShotTargets = (min, max) => {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    let shots = [];
+
+    for (var i = 0; i < 20; i++) {
+      var target = Math.floor(
+        Math.random() * (maxFloored - minCeiled + 1) + minCeiled,
+      );
+      var baseline = lookUpBaselineStrokesGained(target);
+      shots.push({
+        shotNum: i + 1,
+        value: target,
+        baseline: baseline,
+      });
+    }
+    console.log("Attempt Data changed");
+    return shots;
+    };
+
+    console.log("Attempt Data: ", attemptData.requirements)
+
+
+  attemptData.shots = fillShotTargets(100, 150); //current this is getting recalled everytime state changes
+
+  const [outputData, setOutputData] = useState([]);
+  const [toggleResult, setToggleResult] = useState(false);
+
+  const display = () => {
+      if (toggleResult == true) {
+          return <Result submission={outputData} />;
+      } else if (!loading) {
+          return (
+              <Input
+                  outputData={outputData}
+                  attemptData={attemptData}
+                  setToggleResult={setToggleResult}
+                  setOutputData={setOutputData}
+              />
+          );
+      }
+      else {
+          //Loading spinner icon
+         return (<Text>Loading</Text>)
+      }
+  };
+
+  return <PaperProvider>{display()}</PaperProvider>;
 }
