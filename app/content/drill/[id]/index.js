196e65175a1a9eb8a652949508b55d23741f5daa
hannacol <hannacol@oregonstate.edu>
2024-03-05 15:08:06 -0800

    Updated modal to use Lightbox. Uses carousel when in popup

--- app/content/drill/[id]/index.js
+++ app/content/drill/[id]/index.js
@@ -72,7 +72,7 @@ export default function Index() {
           ]}
         />
 
-        {tabComponent()}
+        <ScrollView>{tabComponent()}</ScrollView>
       </SafeAreaView>
     </PaperProvider>
   );
