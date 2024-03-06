3c999cabe9e6d4aa82ab92438401dd7af2798673
hannacol <hannacol@oregonstate.edu>
2024-03-05 20:13:09 -0800

    Fixed "Start Drill" button styling

--- app/content/drill/[id]/index.js
+++ app/content/drill/[id]/index.js
@@ -72,7 +72,7 @@ export default function Index() {
           ]}
         />
 
-        <ScrollView>{tabComponent()}</ScrollView>
+        {tabComponent()}
       </SafeAreaView>
     </PaperProvider>
   );
