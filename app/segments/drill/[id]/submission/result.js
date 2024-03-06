ac5b3f5ec2778b6c32a03548a3de0721fcee551e
Gehrkej <gehrkej@oregonstate.edu>
2024-03-05 18:36:22 -0800

    put scroll view and reset drill button back

--- app/segments/drill/[id]/submission/result.js
+++ app/segments/drill/[id]/submission/result.js
@@ -16,90 +16,80 @@ function Result(props) {
       case "20 Shot Challenge":
         return (
           <>
-            <ScrollView contentContainerStyle={styles.container}>
-              <Text style={styles.sectionTitle}>Drill Results</Text>
+            <Text style={styles.sectionTitle}>Drill Results</Text>
 
-              <View style={styles.dataSection}>
-                <Text style={styles.dataTitle}>Strokes Gained</Text>
-                <View style={styles.dataRow}>
-                  <Text style={styles.dataLabel}>Total: </Text>
-                  <Text style={styles.dataValue}>
-                    {numTrunc(submission["strokesGained"])}
-                  </Text>
-                </View>
-                <View style={styles.dataRow}>
-                  <Text style={styles.dataLabel}>Average: </Text>
-                  <Text style={styles.dataValue}>
-                    {numTrunc(submission["strokesGainedAverage"])}
-                  </Text>
-                </View>
+            <View style={styles.dataSection}>
+              <Text style={styles.dataTitle}>Strokes Gained</Text>
+              <View style={styles.dataRow}>
+                <Text style={styles.dataLabel}>Total: </Text>
+                <Text style={styles.dataValue}>
+                  {numTrunc(submission["strokesGained"])}
+                </Text>
               </View>
-
-              <View style={styles.dataSection}>
-                <Text style={styles.dataTitle}>Average Differences</Text>
-                <View style={styles.dataRow}>
-                  <Icon source={"arrow-up-down"} />
-                  <Text style={styles.dataValue}>
-                    {numTrunc(submission["carryDiffAverage"])}
-                  </Text>
-                </View>
-                <View style={styles.dataRow}>
-                  <Icon source={"arrow-left-right"} />
-                  <Text style={styles.dataValue}>
-                    {numTrunc(submission["sideLandingAverage"])}
-                  </Text>
-                </View>
-                <View style={styles.dataRow}>
-                  <Icon source={"flag"} />
-                  <Text style={styles.dataValue}>
-                    {numTrunc(submission["proxHoleAverage"])}
-                  </Text>
-                </View>
+              <View style={styles.dataRow}>
+                <Text style={styles.dataLabel}>Average: </Text>
+                <Text style={styles.dataValue}>
+                  {numTrunc(submission["strokesGainedAverage"])}
+                </Text>
               </View>
+            </View>
 
-              <View style={styles.chartSection}>
-                <Text style={styles.sectionTitle}>Shot Tendency</Text>
-                <View style={{ ...styles.chartContainer, width: width * 0.8 }}>
-                  <ScatterChart
-                    style={styles.chart}
-                    backgroundColor="#ffffff"
-                    data={[
-                      {
-                        color: "blue",
-                        unit: "%",
-                        values: dots,
-                      },
-                    ]}
-                    horizontalLinesAt={[0]}
-                    verticalLinesAt={[0]}
-                    minY={-10}
-                    maxY={10}
-                    minX={-35}
-                    maxX={35}
-                    chartWidth={width * 0.8}
-                  />
-                </View>
+            <View style={styles.dataSection}>
+              <Text style={styles.dataTitle}>Average Differences</Text>
+              <View style={styles.dataRow}>
+                <Icon source={"arrow-up-down"} />
+                <Text style={styles.dataValue}>
+                  {numTrunc(submission["carryDiffAverage"])}
+                </Text>
               </View>
+              <View style={styles.dataRow}>
+                <Icon source={"arrow-left-right"} />
+                <Text style={styles.dataValue}>
+                  {numTrunc(submission["sideLandingAverage"])}
+                </Text>
+              </View>
+              <View style={styles.dataRow}>
+                <Icon source={"flag"} />
+                <Text style={styles.dataValue}>
+                  {numTrunc(submission["proxHoleAverage"])}
+                </Text>
+              </View>
+            </View>
 
-              <ScrollView>
-                {submission["shots"].map((shot) => (
-                  <ShotAccordion
-                    key={shot["sid"]}
-                    shot={shot}
-                    drill={props.drill}
-                    total={numTrunc(submission["shots"].length)}
-                  />
-                ))}
-              </ScrollView>
+            <View style={styles.chartSection}>
+              <Text style={styles.sectionTitle}>Shot Tendency</Text>
+              <View style={{ ...styles.chartContainer, width: width * 0.8 }}>
+                <ScatterChart
+                  style={styles.chart}
+                  backgroundColor="#ffffff"
+                  data={[
+                    {
+                      color: "blue",
+                      unit: "%",
+                      values: dots,
+                    },
+                  ]}
+                  horizontalLinesAt={[0]}
+                  verticalLinesAt={[0]}
+                  minY={-10}
+                  maxY={10}
+                  minX={-35}
+                  maxX={35}
+                  chartWidth={width * 0.8}
+                />
+              </View>
+            </View>
+
+            <ScrollView>
+              {submission["shots"].map((shot) => (
+                <ShotAccordion
+                  key={shot["sid"]}
+                  shot={shot}
+                  drill={props.drill}
+                  total={numTrunc(submission["shots"].length)}
+                />
+              ))}
             </ScrollView>
-            <Button
-              style={styles.restartButton}
-              mode="contained"
-              buttonColor="#F24E1E"
-              textColor="white"
-            >
-              Restart Drill
-        </Button>
           </>);
       case "Line Test":
         return (<Text>Line Test Result</Text>);
@@ -122,7 +112,19 @@ function Result(props) {
 
   return (
     <>
-      {display()}
+      <ScrollView contentContainerStyle={styles.container}>
+
+
+        {display()}
+      </ScrollView>
+      <Button
+        style={styles.restartButton}
+        mode="contained"
+        buttonColor="#F24E1E"
+        textColor="white"
+      >
+        Restart Drill
+        </Button>
     </>
   );
 }
