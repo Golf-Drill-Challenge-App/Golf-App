892d7bff34a1241f5c08a00b5513363884a4433a
Gehrkej <gehrkej@oregonstate.edu>
2024-03-07 13:17:38 -0800

    Refactored Loading per franks comments

---
+++ components/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View } from 'react-native'
+import { ActivityIndicator } from 'react-native-paper'
+
+const Loading = () => {
+    return (
+        <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+            <ActivityIndicator animating={true} size="large" color="#F24E1E" />
+        </View>
+    )
+}
+
+export default Loading
