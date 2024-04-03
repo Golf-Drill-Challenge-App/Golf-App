22d0c56f53234a4f042fda91643d53c5adabf236
solderq35 <solderq35@gmail.com>
2024-04-03 14:15:21 -0700

    add comment for reference

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,16 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
     plugins: [
+      // Fix issues with test login script (yarn test), and build cache
+      // Reference: https://github.com/expo/router/issues/41#issuecomment-1657674119
+      [
+        "transform-inline-environment-variables",
+        {
+          include: ["EXPO_ROUTER_APP_ROOT"],
+        },
+      ],
       require.resolve("expo-router/babel"),
+      "@babel/plugin-transform-export-namespace-from",
       "react-native-paper/babel",
       "react-native-reanimated/plugin",
       [
