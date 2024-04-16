1132fc0704140bda0518abeea449bd31ef411864
Jake Gehrke <gehrkej@oregonstate.edu>
2024-04-16 10:23:50 -0700

    Restyled navigationRectangle

--- components/input/navigationRectangle.js
+++ components/input/navigationRectangle.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { Icon, Text } from "react-native-paper";
 
 import { getIconByKey } from "~/Utility";
@@ -10,49 +10,89 @@ export default function NavigationRectangle({
   shotIndex,
 }) {
   return (
-    <View style={styles.container}>
-      <View style={styles.rowContainer}>
-        <Text>
+    <View
+      style={{
+        backgroundColor: "#d9d9d9",
+        padding: 20,
+        borderRadius: 10,
+        maxHeight: 250,
+        width: "80%",
+        overflow: "hidden",
+        marginBottom: 20,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        elevation: 5,
+      }}
+    >
+      <View style={{ flex: 1 }}>
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontSize: 25,
+            elevation: 5,
+            textShadowColor: "rgba(0, 0, 0, 0.2)",
+            textShadowOffset: { width: 2, height: 2 },
+            textShadowRadius: 5,
+          }}
+        >
           Shot {shotIndex}/{attemptShots.length}
         </Text>
-        <Text>
-          Target: {attemptShots[shotIndex - 1].target}
-          {drillInfo.requirements[0].distanceMeasure}
-        </Text>
       </View>
-      <View style={styles.rowContainer}>
-        {drillInfo.inputs.map((item, id) => (
-          <View style={styles.horizontalContainer} key={id}>
-            <Icon source={getIconByKey(item.id)} />
-            <Text>
-              {inputValues[item.id]} {item.distanceMeasure}
-            </Text>
-          </View>
-        ))}
+
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <View
+          style={{
+            marginBottom: 10,
+            borderBottomWidth: 2,
+            borderColor: "#A0A0A0",
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 16,
+              padding: 2,
+            }}
+          >
+            Target: {attemptShots[shotIndex - 1].target}
+            {drillInfo.requirements[0].distanceMeasure}
+          </Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginBottom: 10,
+            alignItems: "center",
+          }}
+        >
+          {drillInfo.inputs.map((item, id) => (
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                margin: 10,
+                flex: 1,
+              }}
+              key={id}
+            >
+              <Icon source={getIconByKey(item.id)} size={15} />
+              <Text style={{ fontSize: 13, padding: 2 }}>
+                {inputValues[item.id]} {item.distanceMeasure}
+              </Text>
+            </View>
+          ))}
+        </View>
       </View>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#d9d9d9",
-    padding: 20,
-    borderRadius: 10,
-    maxHeight: 250,
-    width: "80%",
-    overflow: "hidden",
-    marginBottom: 20,
-  },
-  rowContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 10,
-  },
-  title: {
-    fontWeight: "bold",
-  },
-  horizontalContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-});
