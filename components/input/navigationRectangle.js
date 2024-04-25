c0270ac0a22b631bf66e437cbe6e96092165374d
Jake Gehrke <gehrkej@oregonstate.edu>
2024-04-25 11:41:55 -0700

    added ripple when navigationRectangle is pressed

--- components/input/navigationRectangle.js
+++ components/input/navigationRectangle.js
@@ -1,5 +1,11 @@
 import { View } from "react-native";
-import { Divider, Icon, Surface, Text } from "react-native-paper";
+import {
+  Divider,
+  Icon,
+  Surface,
+  Text,
+  TouchableRipple,
+} from "react-native-paper";
 
 import { getIconByKey } from "~/Utility";
 
@@ -8,93 +14,100 @@ export default function NavigationRectangle({
   inputValues,
   shot,
   currentShot,
+  pressFunction,
 }) {
   // console.log("inputValues", inputValues);
   const keys = Object.keys(inputValues);
   return (
     <Surface
       style={{
-        backgroundColor: "#d9d9d9",
-        padding: 10,
-        paddingLeft: 20,
-        paddingRight: 20,
         borderRadius: 10,
-        maxHeight: 250,
         width: "80%",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
         elevation: 5,
       }}
     >
-      <View>
-        <Text
-          style={{
-            fontSize: 20,
-          }}
-        >
-          Shot <Text style={{ fontWeight: "bold" }}>{shot.shotNum}</Text>
-        </Text>
-      </View>
-
-      <View
+      <TouchableRipple
+        onPress={() => {
+          pressFunction();
+        }}
+        rippleColor="rgba(0, 0, 0, 0.2)"
         style={{
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
+          borderRadius: 10,
+          overflow: "hidden",
         }}
       >
         <View
           style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            gap: 10,
+            backgroundColor: "rgba(217, 217, 217, 0.7)",
+            padding: 10,
+            paddingLeft: 20,
+            paddingRight: 20,
+            borderRadius: 10,
+            maxHeight: 250,
+            width: "100%",
+            position: "relative",
           }}
         >
-          {drillInfo.requirements.map((requirement) => (
-            <Text
-              key={requirement.name}
-              style={{
-                fontSize: 16,
-                padding: 2,
-              }}
-            >
-              {requirement.prompt}:
-              <Text style={{ fontWeight: "bold" }}>
-                {" "}
-                {shot.items[requirement.name]} {requirement.distanceMeasure}
-              </Text>
-            </Text>
-          ))}
-        </View>
-
-        <Divider
-          style={{ backgroundColor: "#A0A0A0", height: 1, width: "100%" }}
-        />
-
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-          }}
-        >
-          {currentShot + 1 == shot.shotNum ? (
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center",
-                marginTop: 10,
-              }}
-            >
-              <Text style={{ fontSize: 13, padding: 2, fontWeight: "bold" }}>
-                Current Shot
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <View>
+              <Text
+                style={{
+                  fontSize: 20,
+                }}
+              >
+                Shot <Text style={{ fontWeight: "bold" }}>{shot.shotNum}</Text>
               </Text>
             </View>
-          ) : (
-            drillInfo.inputs.map((input, id) => {
-              if (inputValues[input.id]) {
-                return (
+
+            <View
+              style={{
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  gap: 10,
+                }}
+              >
+                {drillInfo.requirements.map((requirement) => (
+                  <Text
+                    key={requirement.name}
+                    style={{
+                      fontSize: 16,
+                      padding: 2,
+                    }}
+                  >
+                    {requirement.prompt}:
+                    <Text style={{ fontWeight: "bold" }}>
+                      {" "}
+                      {shot.items[requirement.name]}{" "}
+                      {requirement.distanceMeasure}
+                    </Text>
+                  </Text>
+                ))}
+              </View>
+
+              <Divider
+                style={{ backgroundColor: "#A0A0A0", height: 1, width: "100%" }}
+              />
+
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                }}
+              >
+                {currentShot + 1 == shot.shotNum ? (
                   <View
                     style={{
                       flexDirection: "row",
@@ -102,19 +115,40 @@ export default function NavigationRectangle({
                       justifyContent: "center",
                       marginTop: 10,
                     }}
-                    key={id}
                   >
-                    <Icon source={getIconByKey(input.id)} size={15} />
-                    <Text style={{ fontSize: 13, padding: 2 }}>
-                      {inputValues[input.id]} {input.distanceMeasure}
+                    <Text
+                      style={{ fontSize: 13, padding: 2, fontWeight: "bold" }}
+                    >
+                      Current Shot
                     </Text>
                   </View>
-                );
-              }
-            })
-          )}
+                ) : (
+                  drillInfo.inputs.map((input, id) => {
+                    if (inputValues[input.id]) {
+                      return (
+                        <View
+                          style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            marginTop: 10,
+                          }}
+                          key={id}
+                        >
+                          <Icon source={getIconByKey(input.id)} size={15} />
+                          <Text style={{ fontSize: 13, padding: 2 }}>
+                            {inputValues[input.id]} {input.distanceMeasure}
+                          </Text>
+                        </View>
+                      );
+                    }
+                  })
+                )}
+              </View>
+            </View>
+          </View>
         </View>
-      </View>
+      </TouchableRipple>
     </Surface>
   );
 }
