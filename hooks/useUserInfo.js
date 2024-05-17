89477e68743f51b842aa39d576e9f9031e4451f0
ajpert <84763013+ajpert@users.noreply.github.com>
2024-05-17 12:33:14 -0700

    Coach view (#208)
    * assign drills and remove old drills
    
    * styling ish
    
    * wow pretty
    
    * applied most of the comments
    
    * syntax error
    
    * fix assign all
    
    ---------
    
    Co-authored-by: Frankreed <baoanhtdn@gmail.com>

--- hooks/useUserInfo.js
+++ hooks/useUserInfo.js
@@ -1,5 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 import { currentAuthContext } from "~/context/Auth";
 import { db } from "~/firebaseConfig";
 
@@ -7,6 +13,10 @@ export const useUserInfo = ({ userId = null } = {}) => {
   console.log("fetching userInfo: ", { userId });
 
   const { currentTeamId } = currentAuthContext();
+  const week_milliseconds = 604800000;
+  const currentDate = new Date();
+  const currentDateTime = currentDate.getTime();
+
   const { data, error, isLoading } = useQuery({
     queryKey: ["userInfo", { userId }],
     queryFn: async () => {
@@ -14,7 +24,24 @@ export const useUserInfo = ({ userId = null } = {}) => {
         const querySnapshot = await getDoc(
           doc(db, "teams", currentTeamId, "users", userId),
         );
-        return querySnapshot.data();
+        const data = querySnapshot.data();
+        const filteredAssignedData = data.assigned_data.filter((assignment) => {
+          const timeDifference = currentDateTime - assignment.assignedTime;
+          return timeDifference <= week_milliseconds;
+        });
+
+        if (filteredAssignedData.length !== data.assigned_data.length) {
+          const updatedData = {
+            ...data,
+            assigned_data: filteredAssignedData,
+          };
+          await updateDoc(
+            doc(db, "teams", currentTeamId, "users", userId),
+            updatedData,
+          );
+          return updatedData;
+        }
+        return data;
       } else {
         const newUserInfo = {};
         const querySnapshot = await getDocs(
