9819be79c0a6def2bbd172f42f2846479cba8b2a
Frankreed <baoanhtdn@gmail.com>
2024-05-26 16:05:40 -0700

    role in profile disappeared during the merge...

--- components/profileCard.js
+++ components/profileCard.js
@@ -1,8 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import ProfilePicture from "~/components/ProfilePicture";
+import { prettyRole } from "../Constants";
 
 function ProfileCard(props) {
   const profilePicSize = 150;
+
+  const roleColor = props.user["role"] === "owner" ? "#3366ff" : "#222";
 
   const styles = StyleSheet.create({
     container: {
@@ -19,6 +22,9 @@ function ProfileCard(props) {
       fontWeight: "bold",
       marginBottom: 5,
     },
+    role: {
+      fontSize: 16,
+    },
     email: {
       fontSize: 16,
       color: "gray",
@@ -28,6 +34,9 @@ function ProfileCard(props) {
   return (
     <View style={styles.container}>
       <ProfilePicture userInfo={props.user} style={styles.profileImage} />
+      <Text style={[styles.role, { color: roleColor }]}>
+        {prettyRole[props.user["role"]]}
+      </Text>
       <Text style={styles.name}>{props.user["name"]}</Text>
       <Text style={styles.email}>{props.email}</Text>
     </View>
