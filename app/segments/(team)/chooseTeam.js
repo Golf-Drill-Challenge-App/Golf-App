9f45aa842033cbe6ee63fc49a0a016e03651e83e
Jake Gehrke <gehrkej@oregonstate.edu>
2024-05-26 16:25:52 -0700

    updating button styling + display for blacklisted user

--- app/segments/(team)/chooseTeam.js
+++ app/segments/(team)/chooseTeam.js
@@ -2,17 +2,13 @@ import { useQueryClient } from "@tanstack/react-query";
 import { router } from "expo-router";
 import { signOut as signoutFireBase } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
-import {
-  Button,
-  PaperProvider,
-  Surface,
-  TouchableRipple,
-} from "react-native-paper";
+import { Button, PaperProvider } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { themeColors } from "~/Constants";
-import DialogComponent from "~/components/dialog";
-import EmptyScreen from "~/components/emptyScreen";
+import ErrorComponent from "~/components/errorComponent";
+import Loading from "~/components/loading";
 import { currentAuthContext } from "~/context/Auth";
 import { auth, db } from "~/firebaseConfig";
 import { invalidateMultipleKeys } from "~/hooks/invalidateMultipleKeys";
@@ -21,6 +17,12 @@ function ChooseTeam() {
   const { signOut, currentUserId, currentUserInfo, setCurrentUserId } =
     currentAuthContext();
   const queryClient = useQueryClient();
+
+  const [blacklist, setBlacklist] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   async function handleSignOut() {
     try {
       await signoutFireBase(auth);
@@ -31,74 +33,58 @@ function ChooseTeam() {
     }
   }
 
-  const [dialogVisible, setDialogVisible] = useState(false);
-  const hideDialog = () => setDialogVisible(false);
+  useEffect(() => {
+    const fetchBlacklistDoc = async () => {
+      try {
+        const docRef = doc(db, "teams", "1", "blacklist", currentUserId);
+        const docSnap = await getDoc(docRef);
+
+        //See if the user is on blacklist
+        setBlacklist(docSnap.exists());
+      } catch (err) {
+        console.error("Error fetching document: ", err);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBlacklistDoc();
+  }, [currentUserId]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <ErrorComponent errorList={[error]} />;
+  }
 
   return (
     <PaperProvider>
-      <EmptyScreen
-        text={"Choose a team"}
-        postChild={() => (
-          <>
-            <View
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: "center",
+        }}
+      >
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {blacklist ? (
+            <Text
               style={{
-                flexGrow: 1,
-                alignItems: "center",
-                justifyContent: "center",
+                fontSize: 16,
+                textAlign: "center",
+                color: "gray",
               }}
             >
-              <Surface
-                style={{
-                  borderRadius: 10,
-                  elevation: 10,
-                }}
-              >
-                <TouchableRipple
-                  onPress={async () => {
-                    const blacklistDoc = await getDoc(
-                      doc(db, "teams", "1", "blacklist", currentUserId),
-                    );
-                    if (blacklistDoc.exists()) {
-                      setDialogVisible(true);
-                    } else {
-                      await setDoc(
-                        doc(db, "teams", "1", "users", currentUserId),
-                        {
-                          name: currentUserInfo["displayName"],
-                          // hardcoded pfp string for now, add pfp upload to profile settings in future PR
-                          pfp: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-                          // hardcoded "player" role for now, add role selection to profile settings in future PR
-                          role: "player",
-                          uid: currentUserId,
-                          assigned_data: [],
-                          uniqueDrills: [],
-                        },
-                      );
-                      setCurrentUserId(currentUserId);
-                      invalidateMultipleKeys(queryClient, [
-                        ["userInfo", { userId: currentUserId }],
-                      ]);
-                      router.replace("/");
-                      rippleColor = "rgba(0, 0, 0, 0.2)";
-                    }
-                  }}
-                  style={{
-                    backgroundColor: themeColors.accent,
-                    padding: 50,
-                    borderRadius: 10,
-                    overflow: "hidden",
-                  }}
-                >
-                  <Text
-                    style={{
-                      color: themeColors.highlight,
-                    }}
-                  >
-                    Join Team
-                  </Text>
-                </TouchableRipple>
-              </Surface>
-            </View>
+              You've been banned from this team.
+            </Text>
+          ) : (
             <View
               style={{
                 flexGrow: 1,
@@ -107,35 +93,71 @@ function ChooseTeam() {
               }}
             >
               <Button
-                onPress={handleSignOut}
+                onPress={async () => {
+                  await setDoc(doc(db, "teams", "1", "users", currentUserId), {
+                    name: currentUserInfo["displayName"],
+                    // hardcoded pfp string for now, add pfp upload to profile settings in future PR
+                    pfp: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+                    // hardcoded "player" role for now, add role selection to profile settings in future PR
+                    role: "player",
+                    uid: currentUserId,
+                    assigned_data: [],
+                    uniqueDrills: [],
+                  });
+                  setCurrentUserId(currentUserId);
+                  invalidateMultipleKeys(queryClient, [
+                    ["userInfo", { userId: currentUserId }],
+                  ]);
+                  router.replace("/");
+                }}
                 style={{
                   backgroundColor: themeColors.accent,
+                  borderRadius: 12,
+                  marginTop: 20,
                 }}
               >
                 <Text
                   style={{
                     color: themeColors.highlight,
+                    fontSize: 18,
+                    fontSize: 18,
+                    textAlign: "center",
                   }}
                 >
-                  Sign Out
+                  Join Team
                 </Text>
               </Button>
-              <DialogComponent
-                title={"Error!"}
-                content="You have been banned from this team and cannot rejoin."
-                visible={dialogVisible}
-                onHide={hideDialog}
-                buttons={["Ok"]}
-                buttonsFunctions={[
-                  () => {
-                    hideDialog();
-                  },
-                ]}
-              />
             </View>
-          </>
-        )}
-      />
+          )}
+          <View
+            style={{
+              flexGrow: 1,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Button
+              onPress={handleSignOut}
+              style={{
+                backgroundColor: themeColors.accent,
+                borderRadius: 12,
+                marginTop: 20,
+              }}
+            >
+              <Text
+                style={{
+                  color: themeColors.highlight,
+                  fontSize: 18,
+                  fontSize: 18,
+                  textAlign: "center",
+                }}
+              >
+                Sign Out
+              </Text>
+            </Button>
+          </View>
+        </View>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
