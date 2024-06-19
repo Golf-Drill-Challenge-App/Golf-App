3da151fe0956f5e1ad7af0ed17654c7182684e0b
Frank Nguyen <41023671+FrankreedX@users.noreply.github.com>
2024-06-19 11:12:26 -0700

    Frankreed/stroke count (#260)
    * result count for strokes
    
    * chart centering

--- components/resultScreen.js
+++ components/resultScreen.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import {
   ScrollView,
   StyleSheet,
@@ -8,6 +7,7 @@ import {
 } from "react-native";
 import { Icon } from "react-native-paper";
 import ScatterChart from "react-native-scatter-chart";
+import { BarChart, Grid } from "react-native-svg-charts";
 import { prettyTitle, themeColors } from "~/Constants";
 import { numTrunc } from "~/Utility";
 import ErrorComponent from "~/components/errorComponent";
@@ -22,9 +22,6 @@ export default function ResultScreen({
   attemptId = null,
   attemptData = null,
 }) {
-  useEffect(() => {
-    console.log("rendering ResultScreen for: ", attemptId);
-  });
   const { width } = useWindowDimensions();
 
   const {
@@ -52,11 +49,11 @@ export default function ResultScreen({
     return <ErrorComponent errorList={[drillInfoError, attemptError]} />;
   }
 
+  const attempt = attemptId ? fetchedAttempt : attemptData;
+
   const displayShotTendency = drillInfo.outputs.some(
     (output) => output === "carry" || output === "sideLanding",
   );
-
-  const attempt = attemptId ? fetchedAttempt : attemptData;
 
   const dots = attempt["shots"].map((value) => [
     (value["sideLanding"] ? Number(value["sideLanding"]) : 0) + 0.0612,
@@ -74,6 +71,43 @@ export default function ResultScreen({
   xMax += 0.1 * xMax;
   let xMin = Math.min(...xValues, -20);
   xMin += 0.1 * xMin;
+
+  const displayStrokeCount = drillInfo.outputs.some(
+    (output) => output === "strokes",
+  );
+
+  const temp = attempt.shots.reduce((acc, { strokes }) => {
+    acc[strokes] = acc[strokes] || { value: 0, label: strokes };
+    acc[strokes].value += 1;
+    return acc;
+  }, {});
+
+  const barData = Object.values(temp);
+
+  const barWidth = 25;
+  const chartHeight = barData.length * barWidth;
+
+  const Labels = ({ x, y, bandwidth, data }) => {
+    const textArray = data.map((item, index) => {
+      const value = item.value;
+      return (
+        <Text
+          key={index}
+          style={{
+            position: "absolute",
+            top: y(index) + bandwidth / 2 - 9,
+            left: x(value) + 5,
+          }}
+          fontSize={14}
+          fill={"black"}
+          zIndex={0}
+        >
+          {value}
+        </Text>
+      );
+    });
+    return <View>{textArray}</View>;
+  };
 
   const sortedAggOutputs = Object.keys(drillInfo.aggOutputs).sort((a, b) => {
     return prettyTitle[a].localeCompare(prettyTitle[b]);
@@ -151,6 +185,61 @@ export default function ResultScreen({
                 </View>
               </View>
             </>
+          )}
+          {displayStrokeCount && (
+            <View style={{}}>
+              <Text style={styles.sectionTitle}>Stroke Count</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  height: chartHeight,
+                  width: width * 0.9,
+                  margin: "auto", //center the chart
+                }}
+              >
+                {/*YAxis*/}
+                <View
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    height: chartHeight,
+                  }}
+                >
+                  {barData.map((item, index) => (
+                    <Text
+                      key={index}
+                      style={{
+                        position: "absolute",
+                        top: index * barWidth + barWidth / 2 - 8,
+                        left: 0,
+                        fontSize: 14,
+                        fill: "black",
+                        zIndex: 0,
+                      }}
+                    >
+                      {item.label}
+                    </Text>
+                  ))}
+                </View>
+                <BarChart
+                  style={{
+                    height: chartHeight,
+                    width: "100%",
+                  }}
+                  data={barData}
+                  horizontal={true}
+                  yAccessor={({ item }) => item.value}
+                  svg={{ fill: "green", zIndex: 1 }}
+                  contentInset={{ right: 25, left: 25 }}
+                  spacing={0.2}
+                  gridMin={0}
+                >
+                  <Grid direction={Grid.Direction.VERTICAL} />
+                  <Labels />
+                </BarChart>
+              </View>
+            </View>
           )}
 
           <Text style={styles.sectionTitle}>Shot History</Text>
