7e922e0ee3445e4ce5bb2acc1eea89a6c776ecd6
Rob Hess <robwhess@gmail.com>
2024-08-01 14:28:00 -0700

    Import Firebase config from environment variables.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,10 +3,20 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
 import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { devConfig } from "~/firebaseApiKey";
+
+export const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
-const app = initializeApp(devConfig);
+const app = initializeApp(firebaseConfig);
 const persistence = getReactNativePersistence(ReactNativeAsyncStorage);
 const auth = initializeAuth(app, { persistence });
 const db = getFirestore(app);
