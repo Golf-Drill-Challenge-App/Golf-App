65b8beb701f8c14376d056584666cfe39c3f6aff
Frank Nguyen <41023671+FrankreedX@users.noreply.github.com>
2024-08-15 20:06:27 +0700

    IWB-4: User is navigated off of chooseTeam once they're accepted through waitlist  (#312)
    * added blacklist hook
    
    * black list feature works
    
    * some unused stuff removal
    
    * waitlist seems to work
    
    * simplified removeBlacklist.js
    
    * pretty
    
    * removed transaction and hope that it works
    
    * fixed useBlackList.js hook firestore error, and made blacklist check functional
    
    * added refresh spinner for chooseTeam.js
    
    * updated invalidateKeys and fixed some styling
    
    * updated faulty invalidateKeys lists
    
    * kinda works lmao
    
    * updated the invalidateKeys again
    
    * navigation off chooseTeam works. Tech debt +1
    
    * added waitlistError to ErrorComponent
    
    * minor comment change
    
    Co-authored-by: Jake Gehrke <91503842+Gehrkej@users.noreply.github.com>
    
    * changed console log wording in removeBlacklist
    
    Co-authored-by: Jake Gehrke <91503842+Gehrkej@users.noreply.github.com>
    
    * removed copied over comment
    
    * updated tiny comment
    
    Co-authored-by: Jake Gehrke <91503842+Gehrkej@users.noreply.github.com>
    
    * removed redundant log
    
    Co-authored-by: Jake Gehrke <91503842+Gehrkej@users.noreply.github.com>
    
    * fix - added email to blacklist record
    
    * fix - added assertion if pfp exists when removing a user
    
    * prevent double navigation to chooseTeam screen
    
    * fixed "invalid-argument"
    
    * added key
    
    * removed redundant remove user fix
    
    * pretty
    
    * pretty
    
    * pretty
    
    * added missing state from merge
    
    * simple fix
    
    ---------
    
    Co-authored-by: Jake Gehrke <91503842+Gehrkej@users.noreply.github.com>
    Co-authored-by: Jake Gehrke <gehrkej@oregonstate.edu>

--- dbOperations/hooks/useUserInfo.js
+++ dbOperations/hooks/useUserInfo.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { router } from "expo-router";
+import { router, useSegments } from "expo-router";
 import {
   collection,
   doc,
@@ -18,6 +18,7 @@ export const useUserInfo = ({
   role = null,
   enabled = true,
 } = {}) => {
+  const segments = useSegments();
   const { currentTeamId, currentUserId, currentUserVerified } =
     useAuthContext();
   const week_milliseconds = 604800000;
@@ -44,8 +45,10 @@ export const useUserInfo = ({
           console.log("e", getErrorString(e));
         }
 
+        const inChooseTeam = segments.at(-1) === "chooseTeam";
+
         if (!data || !currentUserVerified) {
-          if (currentUserId === userId) {
+          if (currentUserId === userId && !inChooseTeam) {
             router.replace("segments/(team)/chooseTeam");
           }
           return {
@@ -57,6 +60,10 @@ export const useUserInfo = ({
             uniqueDrills: [],
           };
         }
+        if (inChooseTeam) {
+          router.replace("content/assignments");
+        }
+
         const filteredAssignedData = data.assigned_data.filter((assignment) => {
           const timeDifference = currentDateTime - assignment.assignedTime;
           return timeDifference <= week_milliseconds;
