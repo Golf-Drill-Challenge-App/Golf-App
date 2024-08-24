607e52e50696362c463404a9ba2cf3ac294f3153
Frankreed <baoanhtdn@gmail.com>
2024-08-23 17:38:12 -0700

    grammar

--- app/segments/(team)/blacklist.js
+++ app/segments/(team)/blacklist.js
@@ -44,7 +44,7 @@ function Blacklist() {
       <EmptyScreen
         invalidateKeys={invalidateKeys}
         text={
-          "No users found on blacklist. \n To ban a user, select 'Ban' in the top right of their \n profile the from team page."
+          "No users found on blacklist. \n To ban a user, select 'Ban' in the top right of their \n profile from the team page."
         }
       />
     );
